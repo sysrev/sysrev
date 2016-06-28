9f97c0908e0bd90ccb72d2dccc6dcdd4d9f456f2
Zak <pattersonzak@gmail.com>
2016-06-27 22:52:08 -0400

    Clean up code, add configuration to flatten meaningless tag.

--- src/main/scala/co/insilica/sysrev/Importer.scala
+++ src/main/scala/co/insilica/sysrev/Importer.scala
@@ -2,10 +2,12 @@ package co.insilica
 package sysrev
 
 import Implicits._
+import co.insilica.dataProvider.mongo.bson.XmlToBsonConfig
 import dataProvider.mongo.{InsilicaCollection, connect}
 import dataProvider.mongo.connection.Implicits.config._
 import dataProvider.mongo.bson.fromXml._
 import reactivemongo.api.commands.WriteResult
+import reactivemongo.bson.{BSONString, BSONValue}
 
 import scala.concurrent.{Future, ExecutionContext}
 import scala.xml.{Elem, Node, NodeSeq, XML}
@@ -15,25 +17,26 @@ import Scalaz._
 
 object Importer{
 
+  /**
+    * This dataset wraps a lot of text data fields with <style></style> tags.
+    */
+  implicit def configuration = new XmlToBsonConfig {
+    override def knownPaths: Map[String, (Node) => BSONValue] = Map(
+      "style" -> (n => BSONString(n.text))
+    )
+  }
 
+
+  /**
+    * Import systemreview data into the mongo collection
+    */
   def importData(implicit ec: ExecutionContext): Future[Option[List[WriteResult]]] = {
-    val collection: Future[InsilicaCollection] = connect("sysrev")
-
-    val o : Option[Future[List[WriteResult]]] = config.citationsURL.map { data =>
-
-      val xml: Future[Elem] = Future(XML.load(data))
-
-      val docs: Future[List[Node]] = xml.map(x => (x \\ "record").toList)
-
-      // TODO: Trying to insert the entire document at once, doesn't work.
-      // Need to chunk the records up.
-      val res : Future[List[WriteResult]] = for {
-        c <- collection
-        ds <- docs
+    val o : Option[Future[List[WriteResult]]] = config.citationsURL.map { fileUrl =>
+      for {
+        c <- connect("sysrev")
+        ds <- Future((XML.load(fileUrl) \\ "record").toList)
         r <- ds.map(c.insert(_: Node)).sequenceU
       } yield r
-
-      res
     }
 
     o.sequenceU
