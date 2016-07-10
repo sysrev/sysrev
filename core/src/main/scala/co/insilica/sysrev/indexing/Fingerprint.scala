496e7cc7bfdf20c2ebfe8c6504ac21a1769762d3
Zak <pattersonzak@gmail.com>
2016-07-10 14:36:06 -0400

    JDBC connection with multiple dataframes joined together

--- core/src/main/scala/co/insilica/sysrev/indexing/Fingerprint.scala
+++ core/src/main/scala/co/insilica/sysrev/indexing/Fingerprint.scala
@@ -6,6 +6,7 @@ import org.apache.spark.sql.{DataFrame, Row}
 
 import scalaz._
 import Scalaz._
+import Implicits.config.pg
 
 object Fingerprint {
   import spark.readers._
@@ -13,22 +14,45 @@ object Fingerprint {
   def sysrevTfidf: SQLTransaction[Array[Row]] = withSqlContext map { context =>
     import context.implicits._
 
-    val sentenceData = context.createDataFrame(Seq(
-      (0, "Hi I heard about Spark"),
-      (0, "I wish Java could use case classes"),
-      (1, "Logistic regression models are neat")
-    )).toDF("label", "sentence")
+    //    select primary_title, secondary_title, abstract, answer
+    //    from article
+    //      left join article_criteria using (article_Id)
+    //    left join criteria using (criteria_id)
+    //    where name = 'overall include' or name is null
 
-    val tokenizer = new Tokenizer().setInputCol("sentence").setOutputCol("words")
-    val wordsData = tokenizer.transform(sentenceData)
+    val opts = Map(
+      "url" -> s"jdbc:postgresql://${pg.hostPort}/${pg.dbName}",
+      "driver" -> "org.postgresql.Driver",
+      "user" -> pg.name,
+      "password" -> pg.password
+    )
+
+    val articleDf = context.read
+      .format("jdbc")
+      .options(opts + ("dbtable" -> "article"))
+      .load()
+
+
+    val articleCriteriaDf = context.read.format("jdbc").options(opts + ("dbtable" -> "article_criteria")).load()
+    val criteriaDf = context.read.format("jdbc").options(opts + ("dbtable" -> "criteria")).load()
+    val keywordDf = context.read.format("jdbc").options(opts + ("dbtable" -> "keyword")).load()
+    val articleKeywordDf = context.read.format("jdbc").options(opts + ("dbtable" -> "article_keyword")).load()
+
+    val qdf = articleCriteriaDf
+              .join(articleDf, usingColumn = "article_id")
+              .join(criteriaDf, usingColumn = "criteria_id")
+              .filter("""name = 'overall include' or name is null""")
+
+    val tokenizer = new Tokenizer().setInputCol("abstract").setOutputCol("words")
+    val wordsData = tokenizer.transform(qdf)
     val hashingTF = new HashingTF()
-      .setInputCol("words").setOutputCol("rawFeatures").setNumFeatures(20)
+      .setInputCol("words").setOutputCol("rawFeatures").setNumFeatures(100)
     val featurizedData = hashingTF.transform(wordsData)
     val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
     val idfModel = idf.fit(featurizedData)
     val rescaledData = idfModel.transform(featurizedData)
 
-    rescaledData.select("features", "label").take(10)
+    rescaledData.select("features", "answer").take(50)
   }
 }
 
