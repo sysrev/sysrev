8b9bbe084d31cdb7d57998a6c3c5c89c60866e26
Zak <pattersonzak@gmail.com>
2016-07-25 14:28:01 -0400

    Simplify return shape for page of scored articles.

--- api/src/main/scala/co/insilica/sysrev/api/SysrevServlet.scala
+++ api/src/main/scala/co/insilica/sysrev/api/SysrevServlet.scala
@@ -2,8 +2,9 @@ package co.insilica.sysrev
 package api
 
 import co.insilica.apistack.{ApiStack, ResultWrapSupport}
+import co.insilica.auth.Types.WithAnyId
 import co.insilica.sysrev.indexing.{QueryEnv, DocIndex}
-import co.insilica.sysrev.relationalImporter.Types.{CriteriaId, WithArticleId, ArticleId}
+import co.insilica.sysrev.relationalImporter.Types.{CriteriaId, WithArticleId, ArticleId, WithCriteriaId}
 import co.insilica.sysrev.relationalImporter._
 import doobie.free.connection.ConnectionOp
 
@@ -24,24 +25,18 @@ class SysrevServlet extends ApiStack with FutureSupport with ResultWrapSupport {
 
   implicit val tx: Transactor[Task] = Implicits.transactor
 
-  def getRankedPage(p: Int = 0) : Task[List[WithArticleId[WithScore[ArticleWithoutKeywords]]]] =
-    Queries.rankedArticlesPage(p).transact(tx)
+  def getRankedPage(p: Int = 0) : Task[Map[ArticleId, WithScore[ArticleWithoutKeywords]]] =
+    Queries.rankedArticlesPage(p).transact(tx).map{ xs =>
+      xs.map(WithArticleId.unapply(_).get).toMap
+    }
 
-  getT("/ranking/:page") {
-    params("page").parseInt.toOption.map(getRankedPage).getOrElse(getRankedPage(0))
-  }
+  getT("/ranking/:page")(params("page").parseInt.toOption.map(getRankedPage).getOrElse(getRankedPage(0)))
 
-  getT("/ranking") {
-    getRankedPage(0)
-  }
+  getT("/ranking")(getRankedPage(0))
 
-  getT("/criteria") {
-    Queries.allCriteria().transact(tx)
-  }
+  getT("/criteria")(Queries.allCriteria().transact(tx).map(_.map(WithCriteriaId.unapply(_).get).toMap))
 
-  getT("/search/:text") {
-    Queries.textSearch(params("text")).transact(tx)
-  }
+  getT("/search/:text")(Queries.textSearch(params("text")).transact(tx))
 
   /**
     * Returns a map articleid -> List[criteriaid, boolean]
