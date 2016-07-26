3c1be1f625c3a6adcf3f4f2e7a151bd1e7f4b003
Zak <pattersonzak@gmail.com>
2016-07-26 18:47:44 -0400

    Add authservlet

--- api/src/main/scala/ScalatraBootstrap.scala
+++ api/src/main/scala/ScalatraBootstrap.scala
@@ -8,5 +8,6 @@ import javax.servlet.ServletContext
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext): Unit = {
     context.mount(new SysrevServlet, "/*")
+    context.mount(new AuthServlet, "/auth/*")
   }
 }
