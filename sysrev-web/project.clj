8d1acab93fbc4b99cc29a795f3e5489d56effac5
Zak <pattersonzak@gmail.com>
2016-07-26 18:46:24 -0400

    Upgrade dependencies

--- sysrev-web/project.clj
+++ sysrev-web/project.clj
@@ -2,15 +2,14 @@
   :dependencies [;; Clojure (JVM) libraries
                  ;;
                  [org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/core.async "0.2.385"]
                  ;; REPL
                  [org.clojure/tools.nrepl "0.2.12"]
                  [com.cemerick/piggieback "0.2.1"]
-                 [figwheel-sidecar "0.5.3"]
-                 [binaryage/devtools "0.7.2"]
+                 [figwheel-sidecar "0.5.4-7"]
 
                  ;; ClojureScript libraries
-                 [org.clojure/clojurescript "1.9.36"]
+                 [org.clojure/clojurescript "1.9.93"]
                  [reagent "0.6.0-rc"]
                  [secretary "1.2.3"]
                  [kibu/pushy "0.3.6"]
@@ -62,9 +61,8 @@
                 :source-map "resources/public/out-production/sysrev_web.js.map"
                 :source-map-timestamp true}}]}
   :figwheel {:nrepl-port 7888
-             :nrepl-middleware
              ;; these should work with both Cider and Cursive
-             ["cider.nrepl/cider-middleware"
-              "refactor-nrepl.middleware/wrap-refactor"
-              "cemerick.piggieback/wrap-cljs-repl"]
+             :nrepl-middleware ["cider.nrepl/cider-middleware"
+                                "refactor-nrepl.middleware/wrap-refactor"
+                                "cemerick.piggieback/wrap-cljs-repl"]
              :css-dirs ["resources/public/css"]})
--- sysrev-web/src/cljs/sysrev_web/main.cljs
+++ sysrev-web/src/cljs/sysrev_web/main.cljs
@@ -4,14 +4,11 @@
               [sysrev-web.routes :as routes]
               [sysrev-web.ui.core :as ui :refer [main-content]]
               [pushy.core :as pushy]
-              [reagent.core :as r]
-              [devtools.core :as devtools]))
+              [reagent.core :as r]))
 
 (defonce started
   (pushy/start! history))
 
-(devtools/set-pref! :install-sanity-hints true)
-(if ^boolean goog/DEBUG (devtools/install!))
 
 (defn ^:export run []
   (r/render
