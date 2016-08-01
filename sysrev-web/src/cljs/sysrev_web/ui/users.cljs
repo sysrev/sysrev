460b9116f0f0b723caadefa63745f673b28cfd9b
Zak <pattersonzak@gmail.com>
2016-08-01 11:23:11 -0400

    Add all-users summary display.

--- sysrev-web/src/cljs/sysrev_web/ui/users.cljs
+++ sysrev-web/src/cljs/sysrev_web/ui/users.cljs
@@ -1,4 +1,22 @@
-(ns sysrev-web.ui.users)
+(ns sysrev-web.ui.users
+  (:require [sysrev-web.base :refer [server-data]]
+            [sysrev-web.react.components :refer [link]]
+            [reagent.core :as r]))
+
 
 (defn users []
-  [:div "Users page."])
+  [:div.sixteen.wide.column
+   [:h1 "User Activity Summary"]
+   [:div.ui.cards
+    (->> (:users @server-data)
+         (map-indexed
+           (fn [idx {:keys [user articles]}]
+             (let [u (:t user)
+                   username (:username u)
+                   name (:name u)
+                   display-name (if (empty? name) username name)
+                   num-classified (count articles)]
+               [:div.ui.fluid.card
+                [:div.content
+                 [:div.header display-name]]
+                [:div.content (str num-classified " articles classified")]]))))]])
