5ecf6213a2192b02223dd77fc2ffa0f0e1ffdafc
Zak Patterson <zakpatterson@insilica.co>
2016-08-11 15:15:53 -0400

    Improve setup process to link and build semantic automatically

--- sysrev-web/setup.sh
+++ sysrev-web/setup.sh
@@ -5,3 +5,12 @@ npm install semantic-ui@2.2.2
 lein deps
 lein bower install
 lein cljsbuild once dev
+
+echo "Building semantic"
+cd ./semantic && gulp build
+
+echo "Linking semantic build directory"
+ln -sf ./semantic/dist ./resources/public/semantic
+
+echo "Remember to link ./sysrev.dev.nginx.site into /etc/nginx/sites-enabled"
+echo "Done"
