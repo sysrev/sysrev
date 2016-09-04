4d157c37b9c51fc1f2989a34d7d8ff0a1ce88fb3
Jeff Workman <jeff.workman@gmail.com>
2016-09-03 23:55:20 -0400

    Delete src/cljs-old

--- sysrev-web/src/cljs-old/sysrev_web/forms/validate.cljs
+++
@@ -1,9 +0,0 @@
-(ns sysrev-web.forms.validate)
-
-(defn validate
-  "Validate takes a map, and a map of validator/error message pairs, and returns a map of errors."
-  [data validation]
-  (->> validation
-       (map (fn [[k [func message]]]
-              (when (not (func (k data))) [k message])))
-       (into (hash-map))))
