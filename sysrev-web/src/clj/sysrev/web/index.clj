e699ebc1700947eb2a520522abee0d2426bef476
Zak <pattersonzak@gmail.com>
2016-09-05 10:40:51 -0400

    Fix title tag

--- sysrev-web/src/clj/sysrev/web/index.clj
+++ sysrev-web/src/clj/sysrev/web/index.clj
@@ -4,7 +4,7 @@
 (defn index [& req]
   (page/html5
    [:head
-    [:title "dotastats"]
+    [:title "Systematic Review"]
     [:meta {:charset "utf-8"}]
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
     (page/include-js "/vendor/jquery/dist/jquery.min.js"
