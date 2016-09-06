80d70135ca277e4a88ca0c7357d556e59f8a73fa
Jeff Workman <jeff.workman@gmail.com>
2016-09-05 20:05:24 -0400

    Add counts of label values to project stats map

--- sysrev-web/src/clj/sysrev/db/sysrev.clj
+++ sysrev-web/src/clj/sysrev/db/sysrev.clj
@@ -3,7 +3,7 @@
    [sysrev.db.core :refer [do-query do-execute do-transaction]]
    [sysrev.db.articles :as articles]
    [sysrev.db.users :as users]
-   [sysrev.util :refer [mapify-by-id mapify-group-by-id]]
+   [sysrev.util :refer [map-values mapify-by-id mapify-group-by-id]]
    [honeysql.core :as sql]
    [honeysql.helpers :as sqlh :refer :all :exclude [update]]))
 
@@ -57,7 +57,60 @@
      :double (->> counts (filter #(= % 2)) count)
      :multi (->> counts (filter #(> % 2)) count)}))
 
+(defn sr-label-value-counts
+  "Returns counts of [true, false, unknown] label values for each criteria.
+  true/false can be counted by the labels saved by all users.
+  'unknown' values are counted when the user has set a value for at least one
+  label on the article."
+  []
+  (let [entries
+        (->>
+         (-> (select :a.article_id :ac.criteria_id :ac.user_id :ac.answer)
+             (from [:article :a])
+             (join [:article_criteria :ac]
+                   [:= :ac.article_id :a.article_id])
+             (where [:or
+                     [:= :ac.answer true]
+                     [:= :ac.answer false]])
+             do-query)
+         (mapify-group-by-id :user_id true)
+         (map-values
+          (fn [uentries]
+            (->> (mapify-group-by-id :article_id true uentries)
+                 (map-values
+                  #(map-values (comp first (partial map :answer))
+                               (mapify-group-by-id :criteria_id true %)))))))
+        user-articles (->> (keys entries)
+                           (map
+                            (fn [user-id]
+                              (map (fn [article-id]
+                                     [user-id article-id])
+                                   (keys (get entries user-id)))))
+                           (apply concat))
+        criteria-ids (map :criteria_id (articles/all-criteria))
+        ua-label-value
+        (fn [user-id article-id criteria-id]
+          (get-in entries [user-id article-id criteria-id]))
+        answer-count
+        (fn [criteria-id answer]
+          (->> user-articles
+               (filter
+                (fn [[user-id article-id]]
+                  (= answer (ua-label-value
+                             user-id article-id criteria-id))))
+               count))]
+    (->> criteria-ids
+         (map
+          (fn [criteria-id]
+            [criteria-id
+             {:true (answer-count criteria-id true)
+              :false (answer-count criteria-id false)
+              :unknown (answer-count criteria-id nil)}]))
+         (apply concat)
+         (apply hash-map))))
+
 (defn sr-summary []
   {:articles (sr-article-count)
    :labels (sr-label-counts)
+   :label-values (sr-label-value-counts)
    :conflicts (sr-conflict-counts)})
