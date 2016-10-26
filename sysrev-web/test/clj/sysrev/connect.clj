3e9641c256e488581dc195b3a05d38557f9143e2
Zak <pattersonzak@gmail.com>
2016-10-26 18:23:23 -0400

    Clean up config verification fixture, add to all test namespaces
    ## To make a commit, type your commit message and press CTRL-ENTER. To cancel
    ## the commit, close the window. To sign off on the commit, press CTRL-S.
    ##
    ## You may also reference or close a GitHub issue with this commit.  To do so,
    ## type `#` followed by the `tab` key.  You will be shown a list of issues
    ## related to the current repo.  You may also type `owner/repo#` plus the `tab`
    ## key to reference an issue in a different GitHub repo.

--- sysrev-web/test/clj/sysrev/connect.clj
+++ sysrev-web/test/clj/sysrev/connect.clj
@@ -9,14 +9,17 @@
 (defn config-fixture [f]
   (let [{{postgres-port :port
           dbname :dbname} :postgres
-         profile :profile} env]
-    (if (= profile :test)
-      (if (not (= postgres-port 5432))
-        (if (clojure.string/includes? dbname "_test")
-          (f)
-          (println "Db name must include _test"))
-        (println "Cannot run test with pg on port 5432"))
-      (println ":test profile not loaded"))))
+         profile :profile} env
+        validators [["Test profile must be loaded"
+                       #(= (profile :test))]
+                    ["Cannot run tests with pg on port 5432"
+                       #(not (= postgres-port 5432))]
+                    ["Db name must include _test in configuration"
+                       #(clojure.string/includes? dbname "_test")]]
+        error (->> validators (remove #(-> % second (apply []))) first first)]
+    (if error
+      (println error)
+      (f))))
 
 (use-fixtures :once config-fixture)
 
--- sysrev-web/test/sysrev/test/db.clj
+++ sysrev-web/test/sysrev/test/db.clj
@@ -5,7 +5,10 @@
             [sysrev.db.core :refer [do-query do-execute do-transaction]]
             [sysrev.db.articles :as articles]
             [honeysql.core :as sql]
-            [honeysql.helpers :as sqlh :refer :all :exclude [update]]))
+            [honeysql.helpers :as sqlh :refer :all :exclude [update]]
+            [sysrev.connect :refer [config-fixture]]))
+
+(use-fixtures :once config-fixture)
 
 (deftest db-connected
   (is (completes?
