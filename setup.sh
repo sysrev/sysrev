f1ca929edbde62f90dfa3cb18a7bcf55948786d4
Zak Patterson <zakpatterson@insilica.co>
2017-01-26 11:30:37 -0500

    Add automatic install of semantic-ui setup

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+echo "Making semantic directory"
+mkdir -p resources/public/semantic
+
 npm install
+
+echo "Building semantic into resources/public/semantic"
+cd ./semantic && gulp build
 
 lein deps
 lein bower install
 lein cljsbuild once dev
-
-echo "Building semantic"
-cd ./semantic && gulp build
-
-echo "Linking semantic build directory"
-ln -sf ./semantic/dist ./resources/public/semantic
 
 echo "Remember to link ./sysrev.dev.nginx.site into /etc/nginx/sites-enabled"
 echo "Done"
