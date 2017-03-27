058c8c519eeb864ffb0de97a0224abb551045e2a
Zak <pattersonzak@gmail.com>
2017-03-27 19:40:55 -0400

    Add database queries to link files

---
+++ src/clj/sysrev/db/files.clj
@@ -0,0 +1,54 @@
+(ns sysrev.db.files
+  (:require [sysrev.db.core :as db :refer
+             [do-query do-query-map do-execute do-transaction
+              sql-now to-sql-array to-jsonb sql-cast
+              with-query-cache clear-query-cache
+              with-project-cache clear-project-cache]]
+            [sysrev.db.queries :as q]
+            [sysrev.db.project :refer
+             [project-labels project-overall-label-id project-settings]]
+            [sysrev.shared.util :refer [map-values in?]]
+            [sysrev.shared.labels :refer [cleanup-label-answer]]
+            [sysrev.util :refer [crypto-rand crypto-rand-nth]]
+            [honeysql.core :as sql]
+            [honeysql.helpers :as sqlh :refer :all :exclude [update]]
+            [honeysql-postgres.format :refer :all]
+            [honeysql-postgres.helpers :refer :all :exclude [partition-by]]))
+
+(defrecord Filerec [project-id file-id name content-md5 etag])
+
+(defn insert-file-rec [rec]
+  (-> (insert-into :filestore)
+      (values [rec])
+      (do-execute)))
+
+(defn list-files-for-project [project-id]
+  (->>
+    (-> (select :*)
+        (from :filestore)
+        (where [:= :project-id project-id])
+        (order-by [[:ordering :asc :nulls-first] [:upload-time :asc]])
+        (do-query))
+    (mapv map->Filerec)))
+
+
+(defn file-by-id [id project-id]
+  (->>
+    (-> (select :*)
+        (from :filestore)
+        (where [:and
+                [:= :file-id id]
+                ; Require file owned by given project.
+                [:= :project-id project-id]])
+        (limit 1)
+        (do-query)
+        (first))
+    map->Filerec))
+
+(defn mark-deleted [id project-id]
+  (-> (update :filestore)
+      (sset {:deleted-time (java.util.Date.)})
+      (where [:and
+              [:= :key id]
+              [:= :project-id project-id]])
+      (do-execute)))
