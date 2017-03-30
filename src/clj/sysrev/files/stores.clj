781652ad279cda544cebf8c7c5554204d0a6e313
Zak <pattersonzak@gmail.com>
2017-03-29 22:27:18 -0400

    Add fake filestore that just logs warnings to console

--- src/clj/sysrev/files/stores.clj
+++ src/clj/sysrev/files/stores.clj
@@ -1,9 +1,12 @@
 (ns sysrev.files.stores
   (:require [sysrev.files.aws :as aws]
             [sysrev.config.core :refer [env]]
-            [sysrev.files.store :as store]))
+            [sysrev.files.store :as store]
+            [clojure.tools.logging :as log]))
 
-(defmulti get-store (fn [] (get-in env [:filestore :type])))
+(defmulti get-store
+  "Based on the settings available in env, get a FileStore"
+  (fn [] (get-in env [:filestore :type])))
 
 (defmethod get-store :s3 []
   (-> env
@@ -11,6 +14,21 @@
       aws/map->S3Connection
       aws/->S3Store
       aws/connect))
+
+(defmethod get-store :default []
+  (log/warn "No filestore specified in configuration. Just logging intent.")
+  (reify store/FileStore
+    (save-file [this project-id user-id name file]
+      (log/warn (str "Fakestore: Storing " name " attached to project " project-id)))
+    (make-unique-key [this]
+      (log/warn (str "Made a UUID: "(java.util.UUID/randomUUID))))
+    (list-files-for-project [this project-id]
+      (log/warn (str "No files for project " project-id)))
+    (get-file-by-key [this project-id key]
+      (log/warn (str "No file key " key " found for project " project-id)))
+    (delete-file [this project-id key]
+      (log/warn "Delete file " key " for project " project-id))))
+
 
 (defn store-file [project-id user-id name file]
   (-> (get-store)
