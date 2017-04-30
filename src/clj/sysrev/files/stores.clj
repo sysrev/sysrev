1047be079868790e559a1d3925995f725ecde332
Jeff Workman <jeff.workman@gmail.com>
2017-04-30 00:37:39 -0400

    Prevent filestore stub log spam in dev environment

--- src/clj/sysrev/files/stores.clj
+++ src/clj/sysrev/files/stores.clj
@@ -15,19 +15,24 @@
       aws/->S3Store
       aws/connect))
 
+(defonce log-stub-calls (atom false))
+(defmacro ^:private log-stub [& args]
+  `(when @log-stub-calls
+     (log/warn ~@args)))
+
 (defmethod get-store :default []
-  (log/warn "No filestore specified in configuration. Just logging intent.")
+  (log-stub "No filestore specified in configuration. Just logging intent.")
   (reify store/FileStore
     (save-file [this project-id user-id name file]
-      (log/warn (str "Fakestore: Storing " name " attached to project " project-id)))
+      (log-stub (str "Fakestore: Storing " name " attached to project " project-id)))
     (make-unique-key [this]
-      (log/warn (str "Made a UUID: "(java.util.UUID/randomUUID))))
+      (log-stub (str "Made a UUID: "(java.util.UUID/randomUUID))))
     (list-files-for-project [this project-id]
-      (log/warn (str "No files for project " project-id)))
+      (log-stub (str "No files for project " project-id)))
     (get-file-by-key [this project-id key]
-      (log/warn (str "No file key " key " found for project " project-id)))
+      (log-stub (str "No file key " key " found for project " project-id)))
     (delete-file [this project-id key]
-      (log/warn "Delete file " key " for project " project-id))))
+      (log-stub "Delete file " key " for project " project-id))))
 
 
 (defn store-file [project-id user-id name file]
