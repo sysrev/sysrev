0eb3d662c8d1162c98580c94b1dfc7a8cc9c1373
Jeff Workman <jeff.workman@gmail.com>
2017-07-22 04:15:29 -0400

    Add Less and organize style.css contents (src/less/)
    * ./scripts/build-css
    * ./scripts/watch-css (for dev/figwheel)

--- setup.sh
+++ setup.sh
@@ -2,14 +2,23 @@
 
 set -eu
 
+lessc -v > /dev/null || (echo && echo "Error: lessc needed ('sudo npm install -g less')" && false)
+
 echo "Making semantic directory"
 mkdir -p resources/public/semantic
+echo
 
 echo "Installing semantic through NPM"
 npm install > /dev/null
+echo
 
 echo "Building semantic into resources/public/semantic"
-cd ./semantic && gulp build > /dev/null
+cd ./semantic && gulp build > /dev/null && cd ..
+echo
+
+echo "Building style.css from src/less"
+./scripts/build-css
+echo
 
 echo "Done"
 echo
