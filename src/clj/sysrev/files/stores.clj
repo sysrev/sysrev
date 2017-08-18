f8674bee552931d1c1546c7d5565f2a9162714fa
Zak Patterson <pattersonzak@gmail.com>
2017-08-18 12:41:50 -0400

    Change default store so it only saves and returns file metadata

--- src/clj/sysrev/files/stores.clj
+++ src/clj/sysrev/files/stores.clj
@@ -2,6 +2,7 @@
   (:require [sysrev.files.aws :as aws]
             [sysrev.config.core :refer [env]]
             [sysrev.files.store :as store]
+            [sysrev.files.metadata :as metadata]
             [clojure.tools.logging :as log]))
 
 (defmulti get-store
@@ -15,24 +16,14 @@
       aws/->S3Store
       aws/connect))
 
-(defonce log-stub-calls (atom false))
+(defonce log-stub-calls (atom true))
 (defmacro ^:private log-stub [& args]
   `(when @log-stub-calls
      (log/warn ~@args)))
 
 (defmethod get-store :default []
-  (log-stub "No filestore specified in configuration. Just logging intent.")
-  (reify store/FileStore
-    (save-file [this project-id user-id name file]
-      (log-stub (str "Fakestore: Storing " name " attached to project " project-id)))
-    (make-unique-key [this]
-      (log-stub (str "Made a UUID: "(java.util.UUID/randomUUID))))
-    (list-files-for-project [this project-id]
-      (log-stub (str "No files for project " project-id)))
-    (get-file-by-key [this project-id key]
-      (log-stub (str "No file key " key " found for project " project-id)))
-    (delete-file [this project-id key]
-      (log-stub "Delete file " key " for project " project-id))))
+  (log-stub "No filestore specified in configuration. Using metadata-only.")
+  (metadata/->Metadata))
 
 
 (defn store-file [project-id user-id name file]
