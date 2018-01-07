d53f3ace1c8f7c8f8f34c5416ce4252941935290
Jeff Workman <jeff.workman@gmail.com>
2018-01-07 15:52:44 -0500

    Update deps, switch log4j->logback, clean up project.clj

--- src/clj/sysrev/init.clj
+++ src/clj/sysrev/init.clj
@@ -2,7 +2,17 @@
   (:require [sysrev.db.core :as db :refer [set-active-db! make-db-config]]
             [sysrev.web.core :refer [run-web]]
             [sysrev.config.core :refer [env]]
-            [clojure.tools.logging :as log]))
+            [clojure.tools.logging :as log])
+  (:import [org.slf4j.bridge SLF4JBridgeHandler]))
+
+(defn init-logging []
+  (SLF4JBridgeHandler/removeHandlersForRootLogger)
+  (SLF4JBridgeHandler/install)
+  (log/info "installed SLF4JBridgeHandler")
+  true)
+
+(defonce logging-initialized
+  (init-logging))
 
 (defn start-app [& [postgres-overrides server-port-override only-if-new]]
   (let [{profile :profile} env
