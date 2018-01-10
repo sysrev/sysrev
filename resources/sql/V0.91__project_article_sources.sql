3ec932dd28db8ee26b5e0f98fb2fa8f74989c83c
James <jmborden@gmail.com>
2018-01-10 17:59:44 -0500

    sql for project_source and article_source has been created

---
+++ resources/sql/V0.91__project_article_sources.sql
@@ -0,0 +1,9 @@
+create table project_source (
+  source_id serial primary key,
+  project_id integer references project on delete cascade not null,
+  meta jsonb
+  date_created timestamp with time zone default now() not null);
+
+create table article_source (
+  source_id integer references project_source on delete cascade not null,
+  article_id integer references article on delete cascade not null);
