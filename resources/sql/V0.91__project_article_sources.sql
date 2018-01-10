b37954f72702b738de20e94d2d67955c8eef7cfe
James <jmborden@gmail.com>
2018-01-10 18:10:18 -0500

    Fixed typo in SQL for project_source table

--- resources/sql/V0.91__project_article_sources.sql
+++ resources/sql/V0.91__project_article_sources.sql
@@ -1,7 +1,7 @@
 create table project_source (
   source_id serial primary key,
   project_id integer references project on delete cascade not null,
-  meta jsonb
+  meta jsonb,
   date_created timestamp with time zone default now() not null);
 
 create table article_source (
