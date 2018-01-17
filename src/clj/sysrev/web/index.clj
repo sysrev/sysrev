dda5e14764295b7cf3223f8073dbf4d7c77f5f7c
Jeff Workman <jeff.workman@gmail.com>
2018-01-17 15:37:12 -0500

    Add basic favicon image

--- src/clj/sysrev/web/index.clj
+++ src/clj/sysrev/web/index.clj
@@ -24,6 +24,21 @@
    (format "/css/style.%s.css" theme)
    "https://fonts.googleapis.com/css?family=Open+Sans"])
 
+(defn favicon-headers []
+  (list [:link {:rel "apple-touch-icon"
+                :sizes "180x180"
+                :href "/apple-touch-icon.png"}]
+        [:link {:rel "icon"
+                :type "image/png"
+                :sizes "32x32"
+                :href "/favicon-32x32.png"}]
+        [:link {:rel "icon"
+                :type "image/png"
+                :sizes "16x16"
+                :href "/favicon-16x16.png"}]
+        [:meta {:name "theme-color"
+                :content "#ffffff"}]))
+
 (defn index [& [request]]
   (page/html5
    [:head
@@ -31,6 +46,7 @@
     [:meta {:charset "utf-8"}]
     [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+    (favicon-headers)
     (apply page/include-css (css-paths :theme (user-theme request)))
     (page/include-js "/ga.js")]
    [:body
@@ -47,6 +63,7 @@
     [:meta {:charset "utf-8"}]
     [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+    (favicon-headers)
     (apply page/include-css (css-paths :theme (user-theme request)))]
    [:body
     [:div {:id "app"}
