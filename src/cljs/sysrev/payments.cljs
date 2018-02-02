3d2ade58ca7ae242ca41ac36e05d4eca24c1ec34
James <jmborden@gmail.com>
2018-02-02 14:25:54 -0500

    Stripe form can communicate with stripe.com and provide a token in a promise

--- src/cljs/sysrev/payments.cljs
+++ src/cljs/sysrev/payments.cljs
@@ -2,55 +2,47 @@
   (:require [cljsjs.react-stripe-elements]
             [reagent.core :as r]))
 
-;; for advanced compilation, you may need to use
-;; (r/adapt-react-class (aget js/ReactStripeElements "StripeProvider")
-(def StripeProvider (r/adapt-react-class js/ReactStripeElements.StripeProvider))
-
+;; Stripe elements
 (def Elements (r/adapt-react-class js/ReactStripeElements.Elements))
-
-(def CardElement (r/adapt-react-class js/ReactStripeElements.CardElement))
-(def CardNumberElement (r/adapt-react-class js/ReactStripeElements.CardNumberElement))
-(def CardExpiryElement (r/adapt-react-class js/ReactStripeElements.CardExpiryElement))
 (def CardCVCElement (r/adapt-react-class js/ReactStripeElements.CardCVCElement))
+(def CardElement (r/adapt-react-class js/ReactStripeElements.CardElement))
+(def CardExpiryElement (r/adapt-react-class js/ReactStripeElements.CardExpiryElement))
+(def CardNumberElement (r/adapt-react-class js/ReactStripeElements.CardNumberElement))
 (def PostalCodeElement (r/adapt-react-class js/ReactStripeElements.PostalCodeElement))
+(def StripeProvider (r/adapt-react-class js/ReactStripeElements.StripeProvider))
 
 (def element-style {:base {:color "#424770"
                            :letterSpacing "0.025em"
                            :fontFamily "Source Code Pro, Menlo, monospace"
-                           "::placeholder" { :color "#aab7c4"}}
+                           "::placeholder" {:color "#aab7c4"}}
                     :invalid {:color "#9e2146"}}) 
+
+(def StripeForm
+  (r/adapt-react-class
+   (js/ReactStripeElements.injectStripe
+    (r/reactify-component
+     (r/create-class {:display-name "foo-form"
+                      :render
+                      (fn [this]
+                        [:form {:on-submit (fn [e]
+                                             (.preventDefault e)
+                                             (.then (this.props.stripe.createToken)
+                                                    (fn [payload]
+                                                      (.log js/console payload))))
+                                :class "StripeForm"}
+                         [:label "Card Number"
+                          [CardNumberElement {:style element-style}]]
+                         [:label "Expiration date"
+                          [CardExpiryElement {:style element-style}]]
+                         [:label "CVC"
+                          [CardCVCElement {:style element-style}]]
+                         [:label "Postal code"
+                          [PostalCodeElement {:style element-style}]]
+                         [:button.ui.primary.button "Pay"]])})))))
 
 (defn StripeCardInfo
   []
   [:div.ui.secondary.segment
    [StripeProvider {:apiKey "***REMOVED***"}
-    [Elements [:form {:on-submit (fn [e]
-                                   (.preventDefault e)
-                                   (.log js/console "Form submitted"))
-                      :class "StripeForm"}
-               [:label "Card Number"
-                [CardNumberElement {:onBlur #(.log js/console "I blurred")
-                                    :onChange #(.log js/console "I changed")
-                                    :onFocus #(.log js/console "I focused")
-                                    :onReady #(.log js/console "I'm ready")
-                                    :style element-style} ]]
-               [:label "Expiration date"
-                [CardExpiryElement {:onBlur #(.log js/console "I blurred")
-                                    :onChange #(.log js/console "I changed")
-                                    :onFocus #(.log js/console "I focused")
-                                    :onReady #(.log js/console "I'm ready")
-                                    :style element-style}]]
-               [:label "CVC"
-                [CardCVCElement {:onBlur #(.log js/console "I blurred")
-                                 :onChange #(.log js/console "I changed")
-                                 :onFocus #(.log js/console "I focused")
-                                 :onReady #(.log js/console "I'm ready")
-                                 :style element-style}]]
-               [:label "Postal code"
-                [PostalCodeElement {:onBlur #(.log js/console "I blurred")
-                                    :onChange #(.log js/console "I changed")
-                                    :onFocus #(.log js/console "I focused")
-                                    :onReady #(.log js/console "I'm ready")
-                                    :style element-style}]]
-               [:button.ui.primary.button {:on-click #(.log js/console "I submitted")}
-                "Pay"]]]]])
+    [Elements
+     [StripeForm]]]])
