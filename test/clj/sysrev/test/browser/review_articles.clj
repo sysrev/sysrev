a6009c33a4df73b779073b3cb2b08ce968afd985
James <jmborden@gmail.com>
2018-03-09 15:34:06 -0500

    [browser test fix] wait-until-exists used when setting inclusion values checkbox

--- test/clj/sysrev/test/browser/review_articles.clj
+++ test/clj/sysrev/test/browser/review_articles.clj
@@ -294,12 +294,14 @@
                         included? (contains? (set inclusion-values)
                                              %)]
                     ;; each time a selection is made, the checkboxes
-                    ;; are re-rendered. Need to make sure they are all there here
-                    (taxi/wait-until (fn [] (= (count all-values)
-                                               (count (taxi/find-elements
-                                                       {:xpath (str xpath "/descendant::"
-                                                                    (label-name-xpath "for Inclusion")
-                                                                    "/parent::div/" "/input[@type='checkbox']")})))))
+                    ;; are re-rendered. Need to make sure they are all present
+                    #_(taxi/wait-until (fn [] (= (count all-values)
+                                                 (count (taxi/find-elements
+                                                         {:xpath (str xpath "/descendant::"
+                                                                      (label-name-xpath "for Inclusion")
+                                                                      "/parent::div/" "/input[@type='checkbox']")})))))
+                    ;; addition check for Jenkins server (browser tests with stale elements)
+                    (browser/wait-until-exists inclusion-checkbox)
                     (when (not= (taxi/selected? inclusion-checkbox)
                                 included?)
                       (taxi/click inclusion-checkbox)))
