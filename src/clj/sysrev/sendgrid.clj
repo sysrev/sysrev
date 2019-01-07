089f656cdd26c4b992b5a700923dbc001c31bf0b
James Borden <jmborden@gmail.com>
2019-01-07 12:14:03 -0500

    sendgrid.clj added and customized

---
+++ src/clj/sysrev/sendgrid.clj
@@ -0,0 +1,76 @@
+(ns sysrev.sendgrid
+  (:require [clj-http.client :as client]
+            [environ.core :refer [env]]))
+
+
+(def sendgrid-api-key (or (System/getProperty "SENDGRID_API_URL")
+                          (env :sendgrid-api-key)))
+(def sendgrid-api-url "https://api.sendgrid.com/v3/")
+(def sendgrid-default-from "info@sysrev.com")
+(def sendgrid-default-template-id "536e9128-efde-4d0c-a9bc-56fa1a4b19e6")
+(def sendgrid-asm-group-id 7963)
+;; some code from
+;; https://github.com/Purple-Services/common/blob/dev/src/common/sendgrid.clj
+;; Copyright 2017 Purple Services Inc
+;; MIT License: https://github.com/Purple-Services/common/blob/dev/LICENSE
+
+(def common-opts
+  {:as :json
+   :content-type :json
+   :coerce :always
+   :throw-exceptions false
+   :headers {"Authorization" (str "Bearer " sendgrid-api-key)}})
+
+(defn- request
+  [endpoint & [params headers]]
+  (try (let [resp (-> (client/post (str sendgrid-api-url endpoint)
+                                   (merge-with merge 
+                                               common-opts
+                                               {:form-params params}
+                                               {:headers headers}))
+                      :body)]
+         {:success (not (seq (:errors resp)))
+          :resp resp})
+       (catch Exception e
+         {:success false
+          :resp {:error {:message "Unknown error."}}})))
+
+;; Example usage of :substitutions key
+;; {:%name% "Jerry Seinfield"
+;;  :%planName% "Standard Plan"}
+;; If, in your sendgrid template, you had e.g., "Hi %name%"
+(defn- send-email
+  [to from subject payload & {:keys [substitutions]}]
+  (request "mail/send"
+           (merge {:personalizations [{:to [{:email to}]
+                                       :subject subject
+                                       :substitutions substitutions}]
+                   :from {:email from
+                          :name "SysRev"}
+                   :asm {:group_id sendgrid-asm-group-id}}
+                  payload)))
+
+(defn send-text-email
+  [to subject message
+   & {:keys [from]
+      :or {from sendgrid-default-from}}]
+  (send-email to from subject
+              {:content [{:type "text" :value message}]}))
+
+(defn send-html-email
+  [to subject message
+   & {:keys [from]
+      :or {from sendgrid-default-from}}]
+  (send-email to from subject
+              {:content [{:type "text/html" :value message}]}))
+
+(defn send-template-email
+  [to subject message
+   & {:keys [from template-id substitutions]
+      :or {from sendgrid-default-from
+           template-id sendgrid-default-template-id}}]
+  (send-email to from subject
+              {:content [{:type "text/html" :value message}]
+               :template_id template-id}
+              :substitutions substitutions))
+#_ (send-template-email "james@insilica.co" "This is only a test" "<a href='http://localhost:4061/confirm-email'>Click here to confirm your email</a>")
