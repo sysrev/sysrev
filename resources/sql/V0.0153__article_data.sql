180e231b7d2da67f2c4ac0797351c345bb48e8de
Jeff Workman <jeff.workman@gmail.com>
2019-09-11 10:04:52 -0400

    Combine flyway sql files

--- resources/sql/V0.0153__article_data.sql
+++ resources/sql/V0.0153__article_data.sql
@@ -1,13 +1,20 @@
-CREATE TABLE IF NOT EXISTS article_data (
+CREATE TABLE article_data (
        article_data_id SERIAL PRIMARY KEY,
        article_type text,
        article_subtype text,
        title text not null,
-       external_id integer,
-       content jsonb
+       datasource_name text,
+       external_id jsonb,
+       content jsonb,
+       UNIQUE (datasource_name, external_id)
 );
 
-ALTER TABLE article ADD COLUMN IF NOT EXISTS article_data_id
+ALTER TABLE article ADD COLUMN article_data_id
       INTEGER REFERENCES article_data (article_data_id);
 
-CREATE INDEX IF NOT EXISTS a_adata_id_idx ON article (article_data_id);
+CREATE INDEX ON article (article_data_id);
+
+CREATE INDEX ON article_data (article_type);
+CREATE INDEX ON article_data (article_subtype);
+CREATE INDEX ON article_data (datasource_name);
+CREATE INDEX ON article_data (external_id);
--- src/clj/sysrev/datasource/core.clj
+++ src/clj/sysrev/datasource/core.clj
@@ -74,14 +74,15 @@
   (let [article-ids (q/find :article {:article-data-id nil} :article-id)]
     (log/infof "migrating data for %d articles" (count article-ids))
     (->> (map-indexed (fn [i article-id] [i article-id]) article-ids)
-         (pmap (fn [[i article-id]]
-                 (when (zero? (mod i 10000))
-                   (log/infof "copying article #%d...." i))
-                 (try (copy-legacy-article-content article-id)
-                      (catch Throwable e
-                        (log/warnf "got error on article-id=%d" article-id)
-                        (log/warn (.getMessage e))))))
-         doall)))
+         (map (fn [[i article-id]]
+                (when (zero? (mod i 10000))
+                  (log/infof "copying article #%d...." i))
+                (try (copy-legacy-article-content article-id)
+                     (catch Throwable e
+                       (log/warnf "got error on article-id=%d" article-id)
+                       (log/warn (.getMessage e))))))
+         doall)
+    nil))
 
 (defn migrate-article-data-public-ids
   "Populate `external-id` values for PubMed articles in `article-data` using
