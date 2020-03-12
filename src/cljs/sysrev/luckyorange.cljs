ce54beac55bcfe95283063d8b9d9e5f039994421
Thomas Luechtefeld <tomluec@gmail.com>
2020-03-12 19:41:19 -0400

    luckyorange rebase changes

--- src/cljs/sysrev/luckyorange.cljs
+++ src/cljs/sysrev/luckyorange.cljs
@@ -1,19 +1,18 @@
 (ns sysrev.luckyorange
   (:require [clojure.string :as str]
-            [re-frame.core :refer [reg-event-fx reg-fx subscribe]]
             [reagent.core :as r]
-            [sysrev.action.core :refer [def-action]]))
+            [re-frame.db :refer [app-db]]))
 
-; TODO - we should be able to subscribe to changes in :user/display directly but seems like track! doesn't work on subscribe items
-(def user-display-atom (r/cursor re-frame.db/app-db [:state :identity :email]))
+;; TODO - we should be able to subscribe to changes in :user/display
+;; directly but seems like track! doesn't work on subscribe items
+(def user-display-atom (r/cursor app-db [:state :identity :email]))
 
 (defn send-luckyorange-update [email]
   (let [name (first (str/split @email #"@"))]
     (.log js/console (str "chewy name is: " name " email is " @email))
-    (if @email
-      (do
-        (if-not js/window._loq (set! js/window._loq (clj->js [])))
-        (.log js/console (str "name is: " name " email is " @email))
-        (-> ["custom",{:name name :email @email}] clj->js js/window._loq.push)))))
+    (when @email
+      (if-not js/window._loq (set! js/window._loq (clj->js [])))
+      (.log js/console (str "name is: " name " email is " @email))
+      (-> ["custom",{:name name :email @email}] clj->js js/window._loq.push))))
 
 (r/track! send-luckyorange-update user-display-atom)
