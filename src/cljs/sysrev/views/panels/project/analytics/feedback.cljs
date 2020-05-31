e2aeefd0ef692f685e2cc13fbc026b619bcc5933
Thomas Luechtefeld <tomluec@gmail.com>
2020-05-31 19:41:03 -0400

    minor refactoring

--- src/cljs/sysrev/views/panels/project/analytics/feedback.cljs
+++ src/cljs/sysrev/views/panels/project/analytics/feedback.cljs
@@ -24,17 +24,17 @@
 (setup-panel-state panel [:project :project :analytics :feedback])
 
 (defmethod panel-content [:project :project :analytics :feedback] []
-           (fn [child]
-               [:div.ui.aligned.segment
-                [:h2 "Analytics Feedback"]
-                [:span "Thanks for trying out the new analytics features.  We plan to realease improvements and more analytics in the future including:"]
-                [:ul
-                 [:li [:b"Concordance"] "- Add evaluation for categorical labels."]
-                 [:li [:b"Time"] "- See how quickly your project is getting completed. Estimate how much time is left. Watch reviewer completion rates."]
-                 [:li [:b"Similarity"] "- A visualization of document-document similarity.  Eventually with the option to review and remove duplicates"]
-                 [:li [:b"Compensation"] "- Track and plan project spending."]]
-                [:span "Let us know if you find a problem in the existing workflows, or if you have a new suggestion in the below survey."]
-                [:div {:style {:text-align "center"}}
-                 [:iframe {
-                           :src "https://docs.google.com/forms/d/e/1FAIpQLSebmFD_5X-Dzj8SmEwT_t6T5UkNlM5Cj2n5aLseIl3bNpdO6A/viewform?embedded=true"
-                           :width "640" :height "1100" :frameborder "0" :marginheight "0" :marginwidth "0"} "Loading…"]]]))
+  (fn [child]
+    [:div.ui.aligned.segment
+     [:h2 "Analytics Feedback"]
+     [:span "Thanks for trying out the new analytics features.  We plan to realease improvements and more analytics in the future including:"]
+     [:ul
+      [:li [:b"Concordance"] "- Add evaluation for categorical labels."]
+      [:li [:b"Time"] "- See how quickly your project is getting completed. Estimate how much time is left. Watch reviewer completion rates."]
+      [:li [:b"Similarity"] "- A visualization of document-document similarity.  Eventually with the option to review and remove duplicates"]
+      [:li [:b"Compensation"] "- Track and plan project spending."]]
+     [:span "Let us know if you find a problem in the existing workflows, or if you have a new suggestion in the below survey."]
+     [:div {:style {:text-align "center"}}
+      [:iframe {
+                :src "https://docs.google.com/forms/d/e/1FAIpQLSebmFD_5X-Dzj8SmEwT_t6T5UkNlM5Cj2n5aLseIl3bNpdO6A/viewform?embedded=true"
+                :width "640" :height "1100" :frameborder "0" :marginheight "0" :marginwidth "0"} "Loading…"]]]))
