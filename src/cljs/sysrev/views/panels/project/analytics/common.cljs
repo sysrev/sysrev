0ea230c3e91818f05917892e8f887c34e3afef54
Thomas Luechtefeld <tomluec@gmail.com>
2020-06-02 13:34:00 -0400

    added a common namespace for analytics

---
+++ src/cljs/sysrev/views/panels/project/analytics/common.cljs
@@ -0,0 +1,9 @@
+(ns sysrev.views.panels.project.analytics.common
+  (:require
+    [re-frame.core :refer [subscribe dispatch reg-sub reg-event-db]]
+    [sysrev.state.nav :refer [project-uri]]))
+
+(defn beta-message []
+  (let [project-id @(subscribe [:project/active-url-id])]
+    [:span "Analytics is in Beta - we need your"
+     [:a {:href (project-uri project-id "/analytics/feedback")} " feedback"] "."]))
