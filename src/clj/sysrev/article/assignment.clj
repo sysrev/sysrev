f527d90c21c9a20081d8d867830ce1426e4250c6
Thomas Luechtefeld <tomluec@gmail.com>
2020-07-13 12:21:52 -0400

    faster  queries.  Removed dependence on

--- src/clj/sysrev/article/assignment.clj
+++ src/clj/sysrev/article/assignment.clj
@@ -3,122 +3,12 @@
             [honeysql.core :as sql]
             [honeysql.helpers :as sqlh :refer [merge-where]]
             [sysrev.db.core :as db :refer [do-query with-project-cache]]
+            [honeysql.helpers :as sqlh :refer [select limit from join insert-into values where order-by
+                                               having left-join offset]]
             [sysrev.db.queries :as q]
             [sysrev.article.core :as article]
             [sysrev.project.core :as project]
             [sysrev.util :as util :refer [in? map-values index-by]]))
-
-(defn query-assignment-articles [project-id & [predict-run-id]]
-  (with-project-cache project-id [:label-values :saved :articles predict-run-id]
-    (let [predict-run-id (or predict-run-id (q/project-latest-predict-run-id project-id))
-          [articles labels]
-          (pvalues
-           (-> (q/select-project-articles project-id [:a.article-id])
-               (q/with-article-predict-score predict-run-id)
-               (->> do-query
-                    (index-by :article-id)
-                    (map-values #(assoc % :users [] :score (or (:score %) 0.0)))))
-           (-> (q/select-project-articles project-id [:a.article-id :al.user-id :al.confirm-time])
-               (q/join-article-labels)
-               (q/filter-valid-article-label nil)
-               (->> do-query
-                    (group-by :article-id)
-                    (map-values (fn [x] (let [user-ids (->> x (map :user-id) distinct vec)
-                                              confirmed (->> x (remove #(nil? (:confirm-time %)))
-                                                             (map :user-id) distinct vec)]
-                                          {:users user-ids
-                                           :users-confirmed confirmed}))))))]
-      (merge-with merge articles labels))))
-
-(defn unlabeled-articles [project-id & [predict-run-id articles]]
-  (with-project-cache project-id [:label-values :saved :unlabeled-articles predict-run-id]
-    (->> (vals (or articles (query-assignment-articles project-id predict-run-id)))
-         (filter #(= 0 (count (:users-confirmed %))))
-         (map #(dissoc % :users-confirmed)))))
-
-(defn single-labeled-articles [project-id self-id & [predict-run-id articles]]
-  (with-project-cache
-    project-id [:label-values :saved :single-labeled-articles self-id predict-run-id]
-    (->> (vals (or articles (query-assignment-articles project-id predict-run-id)))
-         (filter #(and (= 1 (count (:users %)))
-                       (= 1 (count (:users-confirmed %)))
-                       (not (in? (:users %) self-id))))
-         (map #(dissoc % :users)))))
-
-(defn fallback-articles [project-id self-id & [predict-run-id articles]]
-  (with-project-cache
-    project-id [:label-values :saved :fallback-articles self-id predict-run-id]
-    (->> (vals (or articles (query-assignment-articles project-id predict-run-id)))
-         (filter #(and (< (count (:users-confirmed %)) 2)
-                       (not (in? (:users %) self-id))))
-         (map #(dissoc % :users)))))
-
-(defn unlimited-articles [project-id self-id & [predict-run-id articles]]
-  (with-project-cache
-    project-id [:label-values :saved :unlimited-articles self-id predict-run-id]
-    (->> (vals (or articles (query-assignment-articles project-id predict-run-id)))
-         (filter #(not (in? (:users %) self-id))))))
-
-(defn- pick-ideal-article
-  "Used by the classify task functions to select an article from the candidates.
-
-  Randomly picks from the top 5% of article entries sorted by `sort-keyfn`."
-  [articles sort-keyfn & [predict-run-id min-random-set]]
-  (let [min-random-set (or min-random-set 100)
-        n-closest (max min-random-set (quot (count articles) 4))]
-    (when-let [article-id (->> articles
-                               (sort-by sort-keyfn <)
-                               (take n-closest)
-                               (#(when-not (empty? %) (util/crypto-rand-nth %)))
-                               :article-id)]
-      (-> (article/get-article article-id :predict-run-id predict-run-id)
-          (dissoc :raw)))))
-
-(defn ideal-unlabeled-article
-  "Selects an unlabeled article to assign to a user for classification."
-  [project-id & [predict-run-id articles]]
-  (let [predict-run-id
-        (or predict-run-id (q/project-latest-predict-run-id project-id))]
-    (pick-ideal-article
-     (unlabeled-articles project-id predict-run-id articles)
-     #(math/abs (- (:score %) 0.5))
-     predict-run-id)))
-
-(defn ideal-single-labeled-article
-  "The purpose of this function is to find articles that have a confirmed
-  inclusion label from exactly one user that is not `self-id`, to present
-  to `self-id` to label the article a second time.
-
-  Articles which have any labels saved by `self-id` (even unconfirmed) will
-  be excluded from this query."
-  [project-id self-id & [predict-run-id articles]]
-  (let [predict-run-id
-        (or predict-run-id (q/project-latest-predict-run-id project-id))]
-    (pick-ideal-article
-     (single-labeled-articles project-id self-id predict-run-id articles)
-     #(math/abs (- (:score %) 0.5))
-     predict-run-id)))
-
-(defn ideal-fallback-article
-  "Selects a fallback (with unconfirmed labels) article to assign to a user."
-  [project-id self-id & [predict-run-id articles]]
-  (let [predict-run-id
-        (or predict-run-id (q/project-latest-predict-run-id project-id))]
-    (pick-ideal-article
-     (fallback-articles project-id self-id predict-run-id articles)
-     #(math/abs (- (:score %) 0.5))
-     predict-run-id)))
-
-(defn ideal-unlimited-article
-  "Selects an article to assign to a user from any the user has not labeled."
-  [project-id self-id & [predict-run-id articles]]
-  (let [predict-run-id
-        (or predict-run-id (q/project-latest-predict-run-id project-id))]
-    (pick-ideal-article
-     (unlimited-articles project-id self-id predict-run-id articles)
-     #(count (:users %))
-     predict-run-id
-     50)))
 
 (defn user-confirmed-today-count [project-id user-id]
   (-> (q/select-project-article-labels project-id true [:al.article-id])
@@ -128,28 +18,135 @@
                     (sql/call "date_trunc" "day" :%now)])
       (->> do-query (map :article-id) distinct count)))
 
+(defn query-ideal-fallback-article [project-id user-id]
+  "this is an emergency case where other ideal article methods fail.
+  Picks a random unlabeled article, or single labeled article"
+  (-> (select :article-id)
+      (from
+        [(->
+           (select :ar.article-id)(from [:article :ar])
+           (where [:and
+                   [:= :project-id project-id]
+                   [:= :ar.enabled true]
+                   [:exists
+                    (-> (select 1)
+                        (from [:article_label :al])
+                        (where [:= :al.article-id :ar.article-id])
+                        (sqlh/group :ar.article-id)
+                        (having [:and
+                                 [:<= :%count.%distinct.user-id 1]
+                                 [:= (sql/call :max (sql/raw ["CASE WHEN user_id = " user-id "THEN 1 ELSE 0 END"])) 0]]))]])
+
+           (limit 30)) :sa])
+      (order-by :%random)
+      (limit 1)
+      do-query
+      (first)))
+
+(defn query-ideal-unlimited-article [project-id user-id]
+  "unlimited setting means each reviewer should review every article.
+  Just pick an article the given user hasn't been reviewed by the given user yet."
+  (->
+    (select :article-id)
+    (from
+      [(->
+         (select :ar.article-id)
+         (from [:article :ar])
+         (where [:and
+                 [:= :project-id project-id]
+                 [:= :ar.enabled true]
+                 [:not [:exists (-> (select 1)(from [:article-label :al])
+                                    (where [:and
+                                            [:= :al.article-id :ar.article-id]
+                                            [:= :user-id user-id]]))]]])
+         (limit 10)) :ul])
+    (order-by :%random)
+    (limit 1)
+    do-query
+    (first)))
+
+(defn query-ideal-single-article [project-id user-id]
+  (-> (select :article-id)
+      (from
+        [(->
+           (select :ar.article-id)(from [:article :ar])
+           (where [:and
+                   [:= :project-id project-id]
+                   [:= :ar.enabled true]
+                   [:exists
+                    (-> (select 1)
+                        (from [:article_label :al])
+                        (where [:= :al.article-id :ar.article-id])
+                        (sqlh/group :ar.article-id)
+                        (having [:and
+                                 [:= :%count.%distinct.user-id 1]
+                                 [:= (sql/call :max (sql/raw ["CASE WHEN user_id = " user-id "THEN 1 ELSE 0 END"])) 0]]))]])
+
+           (limit 30)) :sa])
+      (order-by :%random)
+      (limit 1)
+      do-query
+      (first)))
+
+(defn query-ideal-unlabeled-article [project-id]
+  (let
+    [pred-run (q/project-latest-predict-run-id project-id)]
+    (if pred-run
+      (let [res
+            (-> (select :article-id :val)
+                (from
+                  [(-> (select :ar.article-id :lp.val) (from [:article :ar])
+                       (left-join [:label-predicts :lp] [:= :ar.article-id :lp.article-id])
+                       (where [:and
+                               [:= :project-id project-id]
+                               [:= :ar.enabled true]
+                               [:= :predict-run-id (q/project-latest-predict-run-id project-id)]
+                               [:not [:exists (->
+                                                (select 1)
+                                                (from [:article-label :al])
+                                                (where [:= :al.article-id :ar.article-id]))]]])
+                       (limit 100)) :preds])
+                (order-by :%random)
+                (limit 30)
+                do-query)]
+        (if (empty? res)
+          nil
+          (do
+            (reduce (fn [a b] (if (> (Math/abs (- (or (:val a) 0.0) 0.5))
+                                 (Math/abs (- (or (:val b) 0.0) 0.5))) b a)) res))))
+      (-> (select :article-id)
+          (from
+            [(-> (select :ar.article-id) (from [:article :ar])
+                 (where [:and
+                         [:= :project-id project-id]
+                         [:= :ar.enabled true]
+                         [:not [:exists (->
+                                          (select 1)
+                                          (from [:article-label :al])
+                                          (where [:= :al.article-id :ar.article-id]))]]])
+                 (limit 30)) :sq])
+          (order-by :%random)
+          (limit 1)
+          do-query
+          (first)))))
+
 (defn get-user-label-task [project-id user-id]
   (let [{:keys [second-review-prob unlimited-reviews]
-         :or {second-review-prob 0.5
-              unlimited-reviews false}} (project/project-settings project-id)
-        articles (query-assignment-articles project-id)
-        [unlimited pending unlabeled today-count]
-        (pvalues (when unlimited-reviews
-                   (ideal-unlimited-article project-id user-id nil articles))
-                 (when-not unlimited-reviews
-                   (ideal-single-labeled-article project-id user-id nil articles))
-                 (when-not unlimited-reviews
-                   (ideal-unlabeled-article project-id nil articles))
+         :or   {second-review-prob 0.5
+                unlimited-reviews  false}} (project/project-settings project-id)
+        [unlimited single-label unlabeled today-count]
+        (pvalues (when unlimited-reviews      (query-ideal-unlimited-article project-id user-id))
+                 (when-not unlimited-reviews  (query-ideal-single-article project-id user-id))
+                 (when-not unlimited-reviews  (query-ideal-unlabeled-article project-id))
                  (user-confirmed-today-count project-id user-id))
         [article status]
-        (cond unlimited-reviews        [unlimited :unlimited]
-              (and pending unlabeled)  (if (<= (util/crypto-rand) second-review-prob)
-                                         [pending :single] [unlabeled :unreviewed])
-              pending                  [pending :single]
-              unlabeled                [unlabeled :unreviewed]
-              :else                    (when-let [fallback (ideal-fallback-article
-                                                            project-id user-id nil articles)]
-                                         [fallback :single]))]
+        (cond unlimited-reviews             [unlimited :unlimited]
+              (and single-label unlabeled)  (if (<= (util/crypto-rand) second-review-prob)
+                                              [single-label :single] [unlabeled :unreviewed])
+              single-label                  [single-label :single]
+              unlabeled                     [unlabeled :unreviewed]
+              :else                         (when-let [fallback (query-ideal-fallback-article project-id user-id)]
+                                              [fallback :single]))]
     (when (and article status)
       {:article-id (:article-id article)
        :today-count today-count})))
