9455a8e0dbbbb2b405a3ee2012439193a1ebb72f
Thomas Luechtefeld <tomluec@gmail.com>
2020-07-26 12:33:41 -0400

    changed wording on promotion page a bit

--- src/cljs/sysrev/views/panels/promotion.cljs
+++ src/cljs/sysrev/views/panels/promotion.cljs
@@ -22,30 +22,26 @@
     [:h1 "Sysrev Team Pro - $500 for Reviews"]
     [:p
      "Sysrev will award 10 selected document reviews or systematic reviews $500 for reviewer payments.
-         This promotion will be awarded when teams create new " [:b "Team Pro "] "sysrev accounts.
+         This promotion is available to teams creating " [:b "Team Pro "] "sysrev accounts.
          Apply at the bottom of the page. Applications close on August 31."]
     [:p "Learn more about sysrev document reviews at" [:a {:href "/"} " sysrev.com "]
      "or the " [:a {:href "https://blog.sysrev.com/what-is-sysrev"} "what is sysrev?"] " blog."]
 
     [:h2 "What happens after I apply?"]
-    [:p "We will email acceptance decisions within a week.
-    If accepted, you will sign up for Team Pro and Sysrev will add $500 to your project of choice."]
+    [:p "We will email acceptance decisions within a week."]
 
-    [:h2 "Are there restrictions on how I can use the award?"]
      [:p "If your project has been accepted, we will:"
       [:ul
        [:li "Ask you to sign up for team pro."]
        [:li "Enable the sysrev reviewer payments feature on your account."]
        [:li "Provide $500 to your selected project."]
        ]
-      "Review budget expires after 1 year and individual payments cannot exceed $2. Reviewer payments are subject to
+      "This review budget will expire after 1 year and individual payments cannot exceed $2. Reviewer payments are subject to
       the standard 20% overhead rate."]
 
-   [:h2 "Who is eligible for this award?"]
-   [:p "Awarded applications will be required to sign up for
-   Sysrev team pro at $30/month. This subscription cannot be paid for through the provided credit, which is only meant
-   for reviewer payments. Anybody can apply!"]
-
+   [:h2 "Can I apply?"]
+   [:p "Awarded applications are required to either sign up for Team Pro at $30/month or already have a team pro account.
+    Priority will be given to new team pro accounts, but anybody can apply! "]
     [:h2 "Apply Below"]
     [:iframe {
               :src "https://docs.google.com/forms/d/e/1FAIpQLSfwwGo-rbzxZAurT3CmW0pdpcw82a3kYSQbFpsJ41-o5zpJDw/viewform?embedded=true"
