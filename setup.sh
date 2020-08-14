ab7ecf23558afe986edac5fd2d97635ea4e04aa1
Jeff Workman <jeff.workman@gmail.com>
2020-08-14 08:48:53 -0400

    Add scripts/install-semantic (build cache, no auto-build on install)
    * Caches semantic build outputs to $HOME/.sysrev-cache/semantic
    
    * Modifies tasks/install.js from fomantic-ui to prevent gulp from
      running 'build' task upon 'gulp install'.

--- setup.sh
+++ setup.sh
@@ -2,34 +2,43 @@
 
 set -eu
 
-npm -v > /dev/null || (echo && echo "Error: npm not found (install Node/NPM on system)" && false)
-lessc -v > /dev/null || (echo && echo "Error: lessc not found ('sudo npm install -g less')" && false)
-gulp -v > /dev/null || (echo && echo "Error: gulp not found ('sudo npm install -g gulp')" && false)
+function check_deps () {
+  npm -v > /dev/null || (echo && echo "Error: npm not found (install Node/NPM on system)" && false)
+  lessc -v > /dev/null || (echo && echo "Error: lessc not found ('sudo npm install -g less')" && false)
+  gulp -v > /dev/null || (echo && echo "Error: gulp not found ('sudo npm install -g gulp')" && false)
+  md5sum --version > /dev/null || (echo && echo "Error: md5sum not found (install system package)")
+}
 
-echo "Installing semantic through NPM"
-npm install > /dev/null
-echo
+function install_semantic () {
+  echo -n "Installing semantic through NPM ... "
+  ./scripts/install-semantic reinstall > /dev/null 2> /dev/null
+  echo "done"
+}
 
-echo "Building CSS"
-./scripts/build-all-css
-echo
+function copy_client_semantic () {
+  echo -n "Copying semantic directory to client/ ... "
+  rm -rf client/semantic || true
+  cp -r semantic client/
+  echo "done"
+}
 
-echo "Copying semantic directory to client/"
-rm -rf client/semantic || true
-cp -r semantic client/
-echo
+function install_client () {
+  echo -n "Installing client packages from NPM ... "
+  cd client
+  npm install > /dev/null 2> /dev/null
+  cd ..
+  echo "done"
+}
 
-echo "Installing client packages from NPM"
-cd client
-npm install > /dev/null
-cd ..
-echo
+function install_flyway () {
+  echo -n "Installing flyway ... "
+  ./scripts/install-flyway > /dev/null
+  echo "done"
+}
 
-echo "Installing flyway"
-./scripts/install-flyway > /dev/null
-echo
-
+check_deps
+time install_semantic ; echo
+copy_client_semantic ; echo
+time install_client ; echo
+install_flyway ; echo
 echo "You will need to create file vars.sh (see vars.sh.template) and manually add the required private values."
-echo
-echo "Done"
-echo
