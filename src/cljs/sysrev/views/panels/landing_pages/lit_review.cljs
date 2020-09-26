5c0e24be4b62d45e073d387543ba1120e20d10ae
Thomas Luechtefeld <tomluec@gmail.com>
2020-09-26 17:51:30 -0400

    fixed ns again

--- src/cljs/sysrev/views/panels/landing_pages/lit_review.cljs
+++ src/cljs/sysrev/views/panels/landing_pages/lit_review.cljs
@@ -1,4 +1,4 @@
-(ns sysrev.views.panels.landing_pages.lit-review
+(ns sysrev.views.panels.landing-pages.lit-review
    (:require [cljs-time.core :as time]
              [re-frame.core :refer [subscribe reg-sub]]
              [sysrev.data.core :refer [def-data]]
