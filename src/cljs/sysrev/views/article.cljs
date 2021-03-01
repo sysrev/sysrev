9e25c43b2184209ebf8732d69db2e8928706ac00
Thomas Luechtefeld <tomluec@gmail.com>
2021-02-28 21:49:31 -0500

    changed article.cljs 'show predictions' to a simple table

--- src/cljs/sysrev/views/article.cljs
+++ src/cljs/sysrev/views/article.cljs
@@ -347,39 +347,32 @@
         predictions (subscribe [:article/predictions article-id])]
     (fn []
       (when (seq @predictions)
-        [:div.ui.segment {:id "predictions"}
-         [:h4.ui.header "Predictions"]
-         [:div.ui.cards
-          (doall
-            (for [label (->> @labels
-                             vals
-                             (filter #(contains? predictable-label-types (label->type %)))
-                             (sort-by #(count (get-in % [:definition :all-values]))))]
-              (let [all-values (if (= (label->type label) "boolean")
-                                 ["TRUE" "FALSE"]
-                                 (get-in label [:definition :all-values]))]
-                [:div.card {:key (:label-id label)}
-                 [:div.content
-                  [:div.header (:short-label label)]
-                  [:div.description
-                   [:table.ui.compact.table.fixed
-                    [:thead
-                     [:tr
-                      [:th "Label"]
-                      [:th.right.aligned "Prediction"]]]
-                    [:tbody
-                     (doall
-                       (for [v all-values] ^{:key v}
-                         (let [prediction-value (get-in @predictions [(:label-id label) v])
-                               percentage-value (-> prediction-value (* 100) js/Math.round)]
-                           [:tr
-                            [:td (label-value->display label v)]
-                            [:td.right.aligned
-                             (if true;prediction-value
-                               [:span.ui.text {:class [(when (<= percentage-value 25) "red")
-                                                       (when (>= percentage-value 75) "green")]}
-                                (str percentage-value "%")]
-                               "—")]])))]]]]])))]]))))
+        ;[:div.ui.segment {:id "predictions"}
+        ; [:h4.ui.header "Predictions"]
+        ; [:div.ui.segment {:id "predictions"}
+          [:table.ui.compact.celled.table
+           [:thead [:tr [:th {:colspan 3} [:h4.ui.header "Predictions"]]]]
+           [:thead [:tr [:th [:span "label"]] [:th "value"] [:th "probability"]]]
+           [:tbody
+            (doall
+              (for [label (->> @labels
+                               vals
+                               (filter #(contains? predictable-label-types (label->type %)))
+                               (filter #(:enabled %))
+                               (sort-by #(count (get-in % [:definition :all-values]))))]
+                (let [all-values (if (= (label->type label) "boolean")
+                                   ["TRUE"]
+                                   (get-in label [:definition :all-values]))]
+                  (doall
+                    (for [v all-values] ^{:key v}
+                                        (let [prediction-value (get-in @predictions [(:label-id label) v])
+                                              percentage-value (-> prediction-value (* 100) js/Math.round)]
+                                          [:tr [:td (:short-label label)] [:td v]
+                                           [:td [:span.ui.text {:class [(when (<= percentage-value 25) "red")
+                                                                        (when (>= percentage-value 75) "green")]} percentage-value]]]))))))
+
+
+            ]]))))
 
 (defn ArticleInfo [article-id & {:keys [show-labels? private-view? show-score? context
                                         change-labels-button resolving?]
