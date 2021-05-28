b540280169a199c59fef9e4148178d5947fed920
John Shaffer <jshaffer2112@protonmail.com>
2021-05-27 22:05:29 -0500

    Test that username shows correctly in the clone project modal.

--- components/e2e/test/sysrev/e2e/project_test.clj
+++ components/e2e/test/sysrev/e2e/project_test.clj
@@ -7,6 +7,15 @@
 (use-fixtures :each fixtures/wrap-fixtures)
 
 #_:clj-kondo/ignore
+(deftest test-clone-project
+  (testing "username displays correctly"
+    (e2e/doto-driver driver
+      (e2e/log-in-as "test_user_1@insilica.co")
+      (go (e2e/path "/p/21696"))
+      (click-visible {:id "clone-button"})
+      (e2e/wait-is-visible? [{:fn/has-class "clone-project"} {:fn/has-text "test-user-1"}]))))
+
+#_:clj-kondo/ignore
 (deftest test-project-users
   (testing "usernames are correct"
     (e2e/doto-driver driver
