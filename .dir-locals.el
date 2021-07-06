17aba1b786f44fe93eb85b1063ac3826f3177319
John Shaffer <john@insilica.co>
2021-07-06 14:16:47 -0500

    Use symbol clojure-indent-style for clojure-mode 5.10+.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
-((clojure-mode . ((clojure-indent-style . :always-align)))
+((clojure-mode . ((clojure-indent-style . 'always-align)))
  (clojure-mode . ((cider-clojure-cli-global-options . "-A:dev:test:+default"))))
 
