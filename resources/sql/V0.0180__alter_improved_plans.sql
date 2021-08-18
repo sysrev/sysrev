458177dfc5299a17a4b0688a23e67e55cd8b00ae
Mardo Del Cid <mardodcp@gmail.com>
2021-08-18 14:07:36 -0600

    Added stripe_id unique key in migrations files

--- resources/sql/V0.0180__alter_improved_plans.sql
+++ resources/sql/V0.0180__alter_improved_plans.sql
@@ -2,6 +2,8 @@ ALTER TABLE stripe_plan
 ADD COLUMN product varchar(100),
 ADD COLUMN product_name varchar(100);
 
+CREATE UNIQUE INDEX stripe_plan_id ON stripe_plan (id);
+
 ALTER TABLE plan_user
 ADD COLUMN plan_id text REFERENCES stripe_plan (id),
 ADD COLUMN features_override jsonb,
