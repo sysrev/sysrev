16be008d1c88515e399120cd493ebd18d229a813
John Shaffer <john@insilica.co>
2021-11-12 18:54:42 -0600

    Make sure we have everything that nix-shell needs in the AMI

--- projects/datapub/packer/install-datapub.sh
+++ projects/datapub/packer/install-datapub.sh
@@ -8,12 +8,14 @@ set -oeux
 # nix-collect-garbage removes our nixpkgs pin in default.nix, so we
 # aren't calling it at the end to avoid redownloading it on server start.
 # The ideal fix is to figure out how to make a gcroot for the nixpkgs pin.
+# bash-interactive for nix-shell is also not a gcroot.
 nix-collect-garbage -d
 rm -rf datapub
 mkdir -p datapub/src
 cd datapub
 mv /tmp/datapub.jar /tmp/default.nix /tmp/docker-compose-dev.yml /tmp/run.sh .
 nix-build -A inputDerivation -o gcroots
+nix-shell --run "true"
 sudo docker-compose -f docker-compose-dev.yml up -d
 cd
 sudo chown root:root /tmp/datapub.service
