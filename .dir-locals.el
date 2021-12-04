46228bac92003224f23d510baba629b30c43a692
John Shaffer <john@insilica.co>
2021-12-03 22:47:30 -0600

    Add deftest-browser and deftest-etaoin to cider test forms

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,4 @@
 ((clojure-mode . ((clojure-indent-style . 'always-align)))
- (clojure-mode . ((cider-clojure-cli-global-options . "-A:dev:test:+default"))))
+ (clojure-mode . ((cider-clojure-cli-global-options . "-A:dev:test:+default")))
+ (clojure-mode . ((cider-test-defining-forms . ("deftest" "defspec" "deftest-browser" "deftest-etaoin")))))
 
