0dda1fc2437c5a38b509d0eb809bf7817b990a0c
John Shaffer <john@insilica.co>
2021-12-08 18:23:22 -0600

    Update nix files. Use latest npm

--- default.nix
+++ default.nix
@@ -7,56 +7,21 @@ let
   pkgs = import nixpkgs {};
   inherit (pkgs) fetchurl lib stdenv;
   jdk = pkgs.openjdk8;
-  nodeEnv = (import
-    (fetchurl
-      {
-        url = "https://raw.githubusercontent.com/NixOS/nixpkgs/8e6b3914626900ad8f465c3e3541edbb86a95d41/pkgs/development/node-packages/node-env.nix";
-        sha256 = "1ryglhc8jmnwkg4j008kl0wj1d1wb101rx0i72wz0f45y8wiwkm3";
-      }
-    )
-    {
-      lib = lib;
-      libtool = pkgs.libtool;
-      nodejs = pkgs.nodejs;
-      python2 = pkgs.python2;
-      pkgs = pkgs;
-      runCommand = pkgs.runCommand;
-      stdenv = stdenv;
-      writeTextFile = pkgs.writeTextFile;
-    }
-  );
-  # npm 6 and 7 are incompatible both ways, so we're pinning 6
-  npm = nodeEnv.buildNodePackage {
-    name = "npm";
-    packageName = "npm";
-    version = "6.14.15";
-    src = fetchurl {
-      url = "https://registry.npmjs.org/npm/-/npm-6.14.15.tgz";
-      sha256 = "1iaq949s93kmhgn2f087pn20x791v4rnlmnn55i9ca7b08pznqny";
-    };
-    meta = {
-      description = "a package manager for JavaScript";
-      homepage = https://docs.npmjs.com/;
-      license = "Artistic-2.0";
-    };
-    production = true;
-    bypassCache = true;
-    reconstructLock = true;
-  };
-in
-pkgs.mkShell {
-  buildInputs = with pkgs; [
+in with pkgs;
+mkShell {
+  buildInputs = [
     chromedriver
     chromium
     clj-kondo
     (clojure.override { jdk = jdk; })
+    entr # for ./scripts/watch-css
     (flyway.override { jre_headless = jdk; })
     glibcLocales # postgres and rlwrap (used by clj) need this
     jdk
     (leiningen.override { jdk = jdk; })
     lessc
     nodejs
-    npm
+    nodePackages.npm
     polylith
     postgresql_13
     python39Packages.cfn-lint
@@ -64,11 +29,11 @@ pkgs.mkShell {
     time
   ];
   shellHook = ''
-    export LD_LIBRARY_PATH="${pkgs.dbus.lib}/lib:$LD_LIBRARY_PATH"
-    export POSTGRES_DIRECTORY="${pkgs.postgresql_13}"
+    export LD_LIBRARY_PATH="${dbus.lib}/lib:$LD_LIBRARY_PATH"
+    export POSTGRES_DIRECTORY="${postgresql_13}"
     rm chrome
-    ln -s ${pkgs.chromium}/bin/chromium chrome
+    ln -s ${chromium}/bin/chromium chrome
     rm scripts/clj-kondo
-    ln -s ${pkgs.clj-kondo}/bin/clj-kondo scripts/
+    ln -s ${clj-kondo}/bin/clj-kondo scripts/
   '';
 }
