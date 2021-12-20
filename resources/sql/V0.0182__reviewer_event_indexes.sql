17288aad0e081dc8e6771555595d8eae7c586e07
Jeff Workman <jeff.workman@gmail.com>
2021-12-20 17:56:34 -0500

    Add indexes for cascade fields on reviewer_event
    * Fixes slow delete-source

---
+++ resources/sql/V0.0182__reviewer_event_indexes.sql
@@ -0,0 +1,3 @@
+CREATE INDEX reviewer_event_article_id_idx ON reviewer_event (article_id);
+CREATE INDEX reviewer_event_project_id_idx ON reviewer_event (project_id);
+CREATE INDEX reviewer_event_user_id_idx ON reviewer_event (user_id);
