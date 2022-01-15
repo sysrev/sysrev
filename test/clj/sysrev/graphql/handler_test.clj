860dfc9ed1d254d2f61eb2b8a2d1a43bd979ea3b
John Shaffer <john@insilica.co>
2022-01-14 19:35:55 -0600

    Fix graphql project query test

--- test/clj/sysrev/graphql/handler_test.clj
+++ test/clj/sysrev/graphql/handler_test.clj
@@ -79,7 +79,7 @@
                            x))
                  m))
 
-(deftest ^:kaocha/pending ^:integration project-query
+(deftest ^:integration test-project-query
   (test/with-test-system [system {}]
     (let [{:keys [user-id]} (test/create-test-user system)
           project-name  "Graphql - Project Query Test"
