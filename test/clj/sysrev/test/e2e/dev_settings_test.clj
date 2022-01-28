972b690cc9ec9aa776681f7f16bc10700b184ecb
John Shaffer <john@insilica.co>
2022-01-28 15:36:00 -0600

    Implement dev-settings-test
    * Convert test-dev-settings from https://github.com/insilica/systematic_review/blob/b6b74e959a829d04a778ab97c043f597c39fe77f/test/clj/sysrev/test/etaoin/dev_settings.clj#L20
    * Drop account-mutation-tests as redundant with test-dev-settings

--- test/clj/sysrev/test/e2e/dev_settings_test.clj
+++ test/clj/sysrev/test/e2e/dev_settings_test.clj
@@ -8,127 +8,52 @@
    [sysrev.test.core :as test :refer [graphql-request]]
    [sysrev.test.e2e.account :as account]
    [sysrev.test.e2e.core :as e]
-   [sysrev.user.core :refer [user-by-email]]))
+   [sysrev.user.interface :as user]))
 
-(deftest ^:kaocha/pending ^:e2e happy-path-enable
+(deftest ^:e2e test-dev-settings
   (e/with-test-resources [{:keys [driver system] :as test-resources} {}]
-    (let [{:keys [user-id] :as user} (test/create-test-user system)
-          api-key (-> user :email user-by-email :api-token)]
-      (account/log-in test-resources user)
-      (doto driver
-        (et/click-visible :user-name-link)
-        (et/click-visible :user-settings)
-        ;; user can't enable their dev account
-        (e/wait-exists :enable-dev-account)
-        (-> (ea/disabled? :enable-dev-account) is))
-      ;; user can't access the Sysrev API
+    (let [{:keys [email user-id] :as user} (test/create-test-user system)
+          api-key (:api-token (user/user-by-email email))]
+      (is (seq api-key))
       (is (= "user does not have a have pro account"
              (-> (graphql-request system
                                   [[:__schema [[:mutationType [[:fields [:name]]]]]]]
                                   :api-key api-key)
-                 :resolved_value :data first :message)))
-      ;; user doesn't have a datasource account yet
+                 :resolved_value :data first :message))
+          "Basic plan user can't access the sysrev API")
       (is (= "Account Does Not Exist"
              (-> (read-account {:api-key api-key})
                  :errors
                  first
-                 :message)))
-      ;; user adds a valid stripe card
+                 :message))
+          "Basic plan user doesn't have a datasource account")
+      (account/log-in test-resources user)
+      (e/go test-resources (str "/user/" user-id "/settings"))
+      (testing "Basic plan user can't enable dev account"
+        (doto driver
+          (et/is-wait-exists :enable-dev-account)
+          (-> (ea/disabled? :enable-dev-account) is)
+          ;; Guard against https://github.com/insilica/systematic_review/issues/6
+          (ea/perform-actions (-> (ea/make-mouse-input)
+                                  (ea/add-pointer-click-el (ea/query driver :enable-dev-account))))
+          e/wait-until-loading-completes
+          (ea/wait 1)
+          (et/is-not-exists? :user-api-key)))
       (test/change-user-plan! system user-id "Unlimited_Org_Annual_free")
-      (doto driver
-        ;; user toggles their dev account
-        (et/click-visible :user-name-link)
-        (et/click-visible :user-settings)
-        (et/click-visible "//input[@id='enable-dev-account']/..")
-        ;; check that the ui says the dev account is enabled
-        (-> (e/enabled? driver "//input[@id='enable-dev-account']/parent::div[contains(@class,'checked')]")
-            is))
-      ;; check that the user can access the Sysrev API
+      (testing "Pro plan user can enable dev account and view API key"
+        (doto driver
+          e/refresh
+          (ea/perform-actions (-> (ea/make-mouse-input)
+                                  (ea/add-pointer-click-el (ea/query driver :enable-dev-account))))
+          (et/is-wait-visible :user-api-key)
+          (et/is-wait-visible {:fn/has-text api-key})
+          (et/is-wait-visible "//input[@id='enable-dev-account']/parent::div[contains(@class,'checked')]")))
       (is (-> (graphql-request system
-                               [[:__schema [[:mutationType [[:fields [:name]]]]]]]
+                                 [[:__schema [[:mutationType [[:fields [:name]]]]]]]
                                :api-key api-key)
               (get-in [:data :__schema :mutationType :fields])
-              (->> (medley/find-first #(= "importDataset" (:name %))))))
-      ;; check that the user has an enabled Datasource account
+              (->> (medley/find-first #(= "importDataset" (:name %)))))
+          "Pro user can access the sysrev API")
       (is (= api-key (get-in (read-account {:api-key api-key})
-                             [:data :account :apiKey]))))))
-
-(deftest ^:kaocha/pending ^:e2e account-mutation-tests
-  #_(e/with-test-resources [{:keys [driver system] :as test-resources} {}]
-    (let [user (test/create-test-user system)
-          api-key (-> user :email user-by-email :api-token)
-          new-email (-> (str/split (:email user) #"@") first (str "+alpha@example.com"))
-          user-id (:user-id (user-by-email (:email user)))]
-      (account/log-in test-resources user)
-      ;; change plan and enable dev account
-      (test/change-user-plan! system user-id "Unlimited_Org_Annual_free")
-      (et/click-visible :user-name-link)
-      (et/click-visible :user-settings)
-      (et/click-visible "//input[@id='enable-dev-account']/..")
-      ;; check that the ui says the dev account is enabled
-      (is (e/enabled? "//input[@id='enable-dev-account']/parent::div[contains(@class,'checked')]"))
-      ;; user can login to datasource
-      (let [{:keys [email password]} user
-            login-resp (http/post (str (ds-api/ds-host) "/login")
-                                  {:form-params {:email email
-                                                 :password password}})]
-        (is (get-in login-resp [:cookies "token" :value])))
-      ;; change password on sysrev, login to datasource
-      ;; with new password
-      (et/click-visible :log-out-link)
-      (et/click-visible :log-in-link)
-      (et/click-visible "//a[contains(text(),'Forgot Password?')]")
-      (e/fill "//input[@name='email']" (:email user) :clear? true)
-      (et/click-visible "//button[@name='submit']")
-      (e/exists? "//div[contains(text(),'An email has been sent with a link to reset your password.')]")
-      (e/go (str "/reset-password/" (-> (user-by-email (:email user)) :reset-code)))
-      (e/fill "//input[@name='password']" (str/reverse (:password user)) :clear? true)
-      (et/click-visible "//button[@name='submit']")
-      (account/log-in {:email (:email user)
-                       :password (str/reverse (:password user))})
-      (is (e/exists? :log-out-link))
-      (let [{:keys [email password]} user
-            login-resp (http/post (str (ds-api/ds-host) "/login")
-                                  {:form-params {:email email
-                                                 :password (str/reverse password)}})]
-        (is (get-in login-resp [:cookies "token" :value])))
-      ;; change primary email address
-      (et/click-visible :user-name-link)
-      (et/click-visible :user-email)
-      (et/click-visible "//button[contains(text(),'Add a New')]")
-      (e/fill :new-email-address new-email :clear? true)
-      (et/click-visible :new-email-address-submit)
-      (e/wait-exists (str "//div[contains(@class,'email-unverified') and contains(@class,'label')]"
-                          "/parent::h4[contains(text(),'" new-email "')]"))
-      ;; verify the email
-      (let [verify-code (->> (get-user-emails user-id)
-                             (medley/find-first #(= new-email (:email %)))
-                             :verify-code)]
-        (e/go (str "/user/" user-id "/email/" verify-code))
-        (is (e/exists? (str "//div[contains(@class,'blue') and contains(@class,'label')]"
-                            "/parent::h4[contains(text(),'" new-email "')]"))))
-      ;; login to datasource with new email and password
-      (let [{:keys [password]} user
-            login-resp (http/post (str (ds-api/ds-host) "/login")
-                                  {:form-params {:email new-email
-                                                 :password (str/reverse password)}})]
-        (is (get-in login-resp [:cookies "token" :value])))
-      ;; disable pro account
-      (test/change-user-plan! system user-id "Basic")
-      ;; make sure sysrev graphql access is disabled
-      (is (= "user does not have a have pro account"
-             (-> (graphql-request [[:__schema [[:mutationType [[:fields [:name]]]]]]]
-                                  :api-key api-key)
-                 :resolved_value :data first :message)))
-      ;; make sure user can't use datasource graphql
-      (is (= "api-token is not enabled or authorized for this transaction"
-             (-> (graphql-query [[:__schema [[:mutationType [[:fields [:name]]]]]]]
-                                :api-key api-key)
-                 :data first :message)))
-      ;; make sure user can't login to datasource
-      (let [{:keys [password]} user
-            login-resp (http/post (str (ds-api/ds-host) "/login")
-                                  {:form-params {:email new-email
-                                                 :password (str/reverse password)}})]
-        (is (= "Account is not enabled"
-               (-> login-resp (get-in [:headers "Location"]) (str/split #"error=") second)))))))
+                             [:data :account :apiKey]))
+          "Pro user has a datasource account"))))
