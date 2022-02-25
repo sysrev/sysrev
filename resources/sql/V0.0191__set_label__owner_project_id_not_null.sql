74303aec9a45a237f9edb20c416fcef6e2923fa5
John Shaffer <john@insilica.co>
2022-02-25 08:53:26 -0600

    Don't bring down the site when we set not null on owner_project_id
    This actually still seems to lock the table and bring down the site
    for 30 seconds, but this is in line with our deployment downtime, so
    it's acceptable for now.

--- resources/sql/V0.0191__set_label__owner_project_id_not_null.sql
+++ resources/sql/V0.0191__set_label__owner_project_id_not_null.sql
@@ -1,4 +1,9 @@
+-- Following this to avoid taking down the entire service
+-- https://gist.github.com/jcoleman/1e6ad1bf8de454c166da94b67537758b#not-null-constraints
+
+ALTER TABLE label ADD CONSTRAINT label_owner_project_id_not_null CHECK (owner_project_id IS NOT NULL) NOT VALID;
+
 -- This should not affect any rows, but it's here just in case.
 UPDATE label SET owner_project_id = project_id WHERE owner_project_id IS NULL;
 
-ALTER TABLE label ALTER COLUMN owner_project_id SET NOT NULL;
+ALTER TABLE label VALIDATE CONSTRAINT label_owner_project_id_not_null;
