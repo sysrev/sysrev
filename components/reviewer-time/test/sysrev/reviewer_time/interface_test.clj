9086cc4864cdf2515027fb8d921766a975ec3c27
John Shaffer <jshaffer2112@protonmail.com>
2022-03-10 09:15:13 -0800

    Use idle-duration constant in tests

--- components/reviewer-time/test/sysrev/reviewer_time/interface_test.clj
+++ components/reviewer-time/test/sysrev/reviewer_time/interface_test.clj
@@ -1,7 +1,6 @@
 (ns sysrev.reviewer-time.interface-test
-  (:require
-   [clojure.test :refer :all]
-   [sysrev.reviewer-time.interface :as reviewer-time]))
+  (:require [clojure.test :refer :all]
+            [sysrev.reviewer-time.interface :as reviewer-time]))
 
 (def after-duration (java.time.Duration/ofMinutes 2))
 (def idle-duration (java.time.Duration/ofHours 1))
@@ -82,46 +81,50 @@
                 :start (ldt "2021-07-16T10:32:49")
                 :end (.plus (ldt "2021-07-16T10:32:49") after-duration)}])
            is))
-    (testing "Intervals end after idle-duration"
-      (->> [#:reviewer-event{:article-id 36900004
-                             :created (ldt "2021-07-16T10:12:49")
-                             :project-id 1200002}
-            #:reviewer-event{:article-id 36900004
-                             :created (ldt "2021-07-16T11:32:49")
-                             :project-id 1200002}]
-           to-intervals*
-           (= [{:article-id 36900004
-                :project-id 1200002
-                :last-event-time (ldt "2021-07-16T10:12:49")
-                :start (ldt "2021-07-16T10:12:49")
-                :end (.plus (ldt "2021-07-16T10:12:49") after-duration)}
-               {:article-id 36900004
-                :project-id 1200002
-                :last-event-time (ldt "2021-07-16T11:32:49")
-                :start (ldt "2021-07-16T11:32:49")
-                :end (.plus (ldt "2021-07-16T11:32:49") after-duration)}])
-           is)
-      (->> [#:reviewer-event{:article-id 36900004
-                             :created (ldt "2021-07-16T10:12:49")
-                             :project-id 1200002}
-            #:reviewer-event{:article-id 36900004
-                             :created (ldt "2021-07-16T10:15:49")
-                             :project-id 1200002}
-            #:reviewer-event{:article-id 36900004
-                             :created (ldt "2021-07-16T11:32:49")
-                             :project-id 1200002}]
-           to-intervals*
-           (= [{:article-id 36900004
-                :project-id 1200002
-                :last-event-time (ldt "2021-07-16T10:15:49")
-                :start (ldt "2021-07-16T10:12:49")
-                :end (.plus (ldt "2021-07-16T10:15:49") after-duration)}
-               {:article-id 36900004
-                :project-id 1200002
-                :last-event-time (ldt "2021-07-16T11:32:49")
-                :start (ldt "2021-07-16T11:32:49")
-                :end (.plus (ldt "2021-07-16T11:32:49") after-duration)}])
-           is))
+    (let [start (ldt "2021-07-16T10:12:49")
+          start+3 (.plus start (java.time.Duration/ofMinutes 3))
+          after-idle (-> (.plus start idle-duration)
+                         (.plus (java.time.Duration/ofMinutes 5)))]
+      (testing "Intervals end after idle-duration"
+        (->> [#:reviewer-event{:article-id 36900004
+                               :created start
+                               :project-id 1200002}
+              #:reviewer-event{:article-id 36900004
+                               :created after-idle
+                               :project-id 1200002}]
+             to-intervals*
+             (= [{:article-id 36900004
+                  :project-id 1200002
+                  :last-event-time start
+                  :start start
+                  :end (.plus start after-duration)}
+                 {:article-id 36900004
+                  :project-id 1200002
+                  :last-event-time after-idle
+                  :start after-idle
+                  :end (.plus after-idle after-duration)}])
+             is)
+        (->> [#:reviewer-event{:article-id 36900004
+                               :created start
+                               :project-id 1200002}
+              #:reviewer-event{:article-id 36900004
+                               :created start+3
+                               :project-id 1200002}
+              #:reviewer-event{:article-id 36900004
+                               :created after-idle
+                               :project-id 1200002}]
+             to-intervals*
+             (= [{:article-id 36900004
+                  :project-id 1200002
+                  :last-event-time start+3
+                  :start start
+                  :end (.plus start+3 after-duration)}
+                 {:article-id 36900004
+                  :project-id 1200002
+                  :last-event-time after-idle
+                  :start after-idle
+                  :end (.plus after-idle after-duration)}])
+             is)))
     (testing "Handles a series of combinations correctly"
       (->> [#:reviewer-event{:article-id 36900004
                              :created (ldt "2021-07-16T10:12:49")
