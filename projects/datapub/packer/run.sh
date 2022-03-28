af294eefe30495bc9009d53728a9bc6c6cd26910
John Shaffer <john@insilica.co>
2022-03-28 13:27:18 -0700

    Wait on local server config to exist before starting datapub
    This fixes some unhelpful errors in the logs from the server being
    started before cloud-init has completed.

--- projects/datapub/packer/run.sh
+++ projects/datapub/packer/run.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-set -oeux
+set -eu
 
 PATH=$PATH:/nix/var/nix/profiles/per-user/admin/profile/bin/
 
 sudo docker-compose -f docker-compose-dev.yml start
 sudo mkdir -p /run/postgresql
 sudo chown admin:admin /run/postgresql
+
+while ! test -f "/home/admin/datapub/datapub-config.local.edn"; do
+  sleep 1
+  echo "Waiting on /home/admin/datapub/datapub-config.local.edn to exist"
+done
+
 AWS_REGION=us-east-1 nix-shell --run "java -Xms1536m -Xmx1536m -server -cp datapub.jar clojure.main -m datapub.main"
