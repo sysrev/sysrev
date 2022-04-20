3979819a39fc537655df6a7890623b510e2ba448
John Shaffer <jshaffer2112@protonmail.com>
2022-04-20 16:28:07 -0500

    [graphql-gateway] Forward authorization header

--- projects/graphql-gateway/lambda.js
+++ projects/graphql-gateway/lambda.js
@@ -1,16 +1,30 @@
-const { ApolloGateway } = require('@apollo/gateway');
+const { ApolloGateway, RemoteGraphQLDataSource } = require('@apollo/gateway');
 const { ApolloServer } = require('apollo-server-lambda');
 const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
 const { readFileSync } = require('fs');
 
+// https://www.apollographql.com/docs/apollo-server/deployment/lambda
+
 const supergraphSdl = readFileSync('./supergraph.graphql').toString();
 
+class AuthenticatedDataSource extends RemoteGraphQLDataSource {
+  willSendRequest({ request, context }) {
+    request.http.headers.set('Authorization', context.authorization);
+  }
+}
+
 const gateway = new ApolloGateway({
-  supergraphSdl
+  supergraphSdl,
+  buildService({ name, url }) {
+    return new AuthenticatedDataSource({ url });
+  },
 });
 
 const server = new ApolloServer({
   gateway,
+  context: ({ event }) => {
+    return { authorization: event.headers.authorization || '' };
+  },
   introspection: true,
   plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
 });
