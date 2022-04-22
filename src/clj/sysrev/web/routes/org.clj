358381c176ac3e3ca3d50f2bc6e9c198db019bd2
John Shaffer <john@insilica.co>
2022-04-22 12:10:24 -0500

    Format sysrev.web.routes.org

--- src/clj/sysrev/web/routes/org.clj
+++ src/clj/sysrev/web/routes/org.clj
@@ -26,89 +26,89 @@
 
 (defroutes org-routes
   (context
-   "/api" []
-   (GET "/orgs" request
+    "/api" []
+    (GET "/orgs" request
+      (with-authorize request {:logged-in true}
+        (api/read-orgs (current-user-id request))))
+    (GET "/org/valid-name" request
+      (with-authorize request {:logged-in true}
+        (let [org-name (get-in request [:params :org-name])]
+          (api/validate-org-name org-name))))
+    (POST "/org" request
+      (with-authorize request {:logged-in true}
+        (let [{:keys [org-name]} (:body request)]
+          (api/create-org! (current-user-id request) org-name))))
+    (POST "/org/pro" request
+      (with-authorize request {:logged-in true}
+        (let [{:keys [org-name plan payment-method]} (:body request)]
+          (api/create-org-pro! (current-user-id request) org-name plan payment-method))))
+    (GET "/org/available-plans" request
+      (with-authorize request {:logged-in true}
+        (api/org-available-plans)))
+    (context "/org/:org-id" [org-id :<< as-int :as _request]
+      (GET "/users" request
+        (with-authorize request {}
+          (-> (group/group-id->name org-id)
+              (api/users-in-group))))
+      (POST "/get-share-code" request
+        (with-authorize request {:authorize-fn (org-role? org-id ["admin" "owner"])}
+          {:share-code (group/get-share-hash org-id)}))
+      (POST "/user" request
+        (with-authorize request {:authorize-fn (org-role? org-id ["owner"])}
+          (let [user-id (get-in request [:body :user-id])]
+            (api/set-user-group! user-id (group/group-id->name org-id) true))))
+      (POST "/join" request
         (with-authorize request {:logged-in true}
-          (api/read-orgs (current-user-id request))))
-   (GET "/org/valid-name" request
-        (with-authorize request {:logged-in true}
-          (let [org-name (get-in request [:params :org-name])]
-            (api/validate-org-name org-name))))
-   (POST "/org" request
-         (with-authorize request {:logged-in true}
-           (let [{:keys [org-name]} (:body request)]
-             (api/create-org! (current-user-id request) org-name))))
-   (POST "/org/pro" request
-         (with-authorize request {:logged-in true}
-           (let [{:keys [org-name plan payment-method]} (:body request)]
-             (api/create-org-pro! (current-user-id request) org-name plan payment-method))))
-   (GET "/org/available-plans" request
-        (with-authorize request {:logged-in true}
-          (api/org-available-plans)))
-   (context "/org/:org-id" [org-id :<< as-int :as _request]
-            (GET "/users" request
-                 (with-authorize request {}
-                   (-> (group/group-id->name org-id)
-                       (api/users-in-group))))
-            (POST "/get-share-code" request
-                 (with-authorize request {:authorize-fn (org-role? org-id ["admin" "owner"])}
-                   {:share-code (group/get-share-hash org-id)}))
-            (POST "/user" request
-                  (with-authorize request {:authorize-fn (org-role? org-id ["owner"])}
-                    (let [user-id (get-in request [:body :user-id])]
-                      (api/set-user-group! user-id (group/group-id->name org-id) true))))
-            (POST "/join" request
-                  (with-authorize request {:logged-in true}
-                    (let [user-id (current-user-id request)]
-                      (if (= org-id (:org-id (enc/decrypt-wrapped64 (get-in request [:body :register-hash]))))
-                        (api/set-user-group! user-id (group/group-id->name org-id) true)
-                        {:error {:status 403 :type :project
-                                 :message "Not authorized (project member)"}}))))
-            (PUT "/user" request
-                 (with-authorize request {:authorize-fn (org-role? org-id ["owner"])}
-                   (let [user-id (get-in request [:body :user-id])
-                         permissions (get-in request [:body :permissions])]
-                     (api/set-user-group-permissions! user-id org-id permissions))))
-            (DELETE "/user" request
-                    (with-authorize request {:authorize-fn (org-role? org-id ["owner"])}
-                      (let [user-id (get-in request [:body :user-id])]
-                        (api/set-user-group! user-id (group/group-id->name org-id) false))))
-            (POST "/project" request
-                  (with-authorize request {:authorize-fn (org-role? org-id ["admin" "owner"])}
-                    (let [{:keys [project-name public-access]} (-> request :body)
-                          user-id (current-user-id request)]
-                      (api/create-project-for-org! project-name user-id org-id public-access))))
-            (POST "/project/clone" request
-                  (with-authorize request {:authorize-fn (org-role? org-id ["admin" "owner"])}
-                    (let [{:keys [src-project-id]} (:body request)
-                          user-id (current-user-id request)]
-                      (api/clone-project-for-org! {:src-project-id src-project-id
-                                                   :user-id user-id
-                                                   :org-id org-id}))))
-            (GET "/projects" request
-                 (with-authorize request {}
-                   (api/group-projects org-id :private-projects?
-                                       ((org-role? org-id ["owner" "admin" "member"])
-                                        request))))
-            (context "/stripe" []
-                     (GET "/default-source" request
-                          (with-authorize request
-                            {:authorize-fn (org-role? org-id ["owner" "admin"])}
-                            (api/org-default-stripe-source org-id)))
-                     (POST "/payment-method" request
-                           (with-authorize request
-                             {:authorize-fn (org-role? org-id ["owner" "admin"])}
-                             (let [{:keys [payment_method]} (:body request)]
-                               (api/update-org-stripe-payment-method! org-id payment_method))))
-                     (GET "/current-plan" request
-                          (with-authorize request
-                            {:authorize-fn (org-role? org-id ["owner" "admin" "member"])}
-                            (api/group-current-plan org-id)))
-                     (POST "/subscribe-plan" request
-                           (with-authorize request
-                             {:authorize-fn (org-role? org-id ["owner" "admin"])}
-                             (let [{:keys [plan]} (:body request)]
-                               (api/subscribe-org-to-plan org-id plan))))
-                     (GET "/available-plans" request
-                          (with-authorize request {:authorize-fn (org-role? org-id ["owner" "admin" "member"])}
-                            (api/org-available-plans)))))))
+          (let [user-id (current-user-id request)]
+            (if (= org-id (:org-id (enc/decrypt-wrapped64 (get-in request [:body :register-hash]))))
+              (api/set-user-group! user-id (group/group-id->name org-id) true)
+              {:error {:status 403 :type :project
+                       :message "Not authorized (project member)"}}))))
+      (PUT "/user" request
+        (with-authorize request {:authorize-fn (org-role? org-id ["owner"])}
+          (let [user-id (get-in request [:body :user-id])
+                permissions (get-in request [:body :permissions])]
+            (api/set-user-group-permissions! user-id org-id permissions))))
+      (DELETE "/user" request
+        (with-authorize request {:authorize-fn (org-role? org-id ["owner"])}
+          (let [user-id (get-in request [:body :user-id])]
+            (api/set-user-group! user-id (group/group-id->name org-id) false))))
+      (POST "/project" request
+        (with-authorize request {:authorize-fn (org-role? org-id ["admin" "owner"])}
+          (let [{:keys [project-name public-access]} (-> request :body)
+                user-id (current-user-id request)]
+            (api/create-project-for-org! project-name user-id org-id public-access))))
+      (POST "/project/clone" request
+        (with-authorize request {:authorize-fn (org-role? org-id ["admin" "owner"])}
+          (let [{:keys [src-project-id]} (:body request)
+                user-id (current-user-id request)]
+            (api/clone-project-for-org! {:src-project-id src-project-id
+                                         :user-id user-id
+                                         :org-id org-id}))))
+      (GET "/projects" request
+        (with-authorize request {}
+          (api/group-projects org-id :private-projects?
+                              ((org-role? org-id ["owner" "admin" "member"])
+                               request))))
+      (context "/stripe" []
+        (GET "/default-source" request
+          (with-authorize request
+            {:authorize-fn (org-role? org-id ["owner" "admin"])}
+            (api/org-default-stripe-source org-id)))
+        (POST "/payment-method" request
+          (with-authorize request
+            {:authorize-fn (org-role? org-id ["owner" "admin"])}
+            (let [{:keys [payment_method]} (:body request)]
+              (api/update-org-stripe-payment-method! org-id payment_method))))
+        (GET "/current-plan" request
+          (with-authorize request
+            {:authorize-fn (org-role? org-id ["owner" "admin" "member"])}
+            (api/group-current-plan org-id)))
+        (POST "/subscribe-plan" request
+          (with-authorize request
+            {:authorize-fn (org-role? org-id ["owner" "admin"])}
+            (let [{:keys [plan]} (:body request)]
+              (api/subscribe-org-to-plan org-id plan))))
+        (GET "/available-plans" request
+          (with-authorize request {:authorize-fn (org-role? org-id ["owner" "admin" "member"])}
+            (api/org-available-plans)))))))
