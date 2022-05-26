4f888f6b4c6014418680df191bd3875dcab6ab72
John Shaffer <jdsha@proton.me>
2022-05-26 13:56:16 -0500

    Streamline infra config

--- components/infra/src/sysrev/infra/core.clj
+++ components/infra/src/sysrev/infra/core.clj
@@ -6,8 +6,8 @@
             [salmon.cloudformation.interface :as cfn]
             [salmon.signal.interface :as sig]
             [sysrev.infra.datapub :as datapub]
-            [sysrev.infra.graphql-gateway :as graphql-gateway]
             [sysrev.infra.global :as global]
+            [sysrev.infra.graphql-gateway :as graphql-gateway]
             [sysrev.infra.regional :as regional]
             [sysrev.infra.sysrev :as sysrev]))
 
@@ -18,69 +18,68 @@
 (defn config-val [k]
   (ds/ref [:common :config k]))
 
-(defn global-output [k]
-  (ds/ref [:common :global-resources :outputs k]))
+(def global-outputs (ds/ref [:common :global-resources :outputs]))
 
-(defn regional-output [k]
-  (ds/ref [:common :regional-resources :outputs k]))
+(def regional-outputs (ds/ref [:common :regional-resources :outputs]))
 
 (defn merge-maps [& maps]
   {::ds/config {:maps maps}
    ::ds/start (fn [{:keys [maps]}]
                 (reduce merge {} maps))})
 
+(defn call [f & args]
+  {::ds/config {:args args :f f}
+   ::ds/start (fn [{:keys [args f]}]
+                (apply f args))})
+
 (defn system-map []
   {::ds/base {:salmon/pre-validate sig/pre-validate-conf}
    ::ds/defs
    {:common
-    {:config (-> "cloudformation-config.edn" io/resource slurp edn/read-string)
+    {:config (call #(-> % io/resource slurp edn/read-string)
+                   "cloudformation-config.edn")
+     :global-params
+     (merge-maps {:Env (config-val :Env)}
+                 (config-val :global))
      :global-resources
      (cfn/stack {:capabilities capabilities
                  :lint? true
                  :name "Sysrev-Global-Resources"
-                 :parameters {:DatapubZoneApex (config-val :datapub-zone-apex)
-                              :Env (config-val :env)
-                              :InsilicaZoneApex (config-val :insilica-zone-apex)
-                              :SysrevZoneApex (config-val :sysrev-zone-apex)}
+                 :parameters (ds/ref [:common :global-params])
                  :template global/template})
+     :regional-params
+     (call (fn [outputs]
+             (-> outputs
+                 (select-keys [:CloudFrontOAI :CodeBucket :DatapubHostedZoneId
+                               :DatapubZoneApex :SysrevHostedZoneId :SysrevZoneApex])
+                 (assoc :NumberOfAZs 6)))
+           global-outputs)
      :regional-resources
      (cfn/stack {:capabilities capabilities
                  :lint? true
                  :name "Sysrev-Regional-Resources"
-                 :parameters {:CloudFrontOAI (global-output :CloudFrontOAI)
-                              :CodeBucket (global-output :CodeBucket)
-                              :DatapubHostedZoneId (global-output :DatapubHostedZoneId)
-                              :DatapubZoneApex (global-output :DatapubZoneApex)
-                              :NumberOfAZs 6
-                              :SysrevHostedZoneId (global-output :SysrevHostedZoneId)
-                              :SysrevZoneApex (global-output :SysrevZoneApex)}
+                 :parameters (ds/ref [:common :regional-params])
                  :template regional/template})}
     :datapub
-    {:files-domain-name
-     {::ds/config {:apex (global-output :DatapubZoneApex)}
-      ::ds/start (fn [config]
-                   (str "files." (:apex config)))}
-     :params
-     (merge-maps
-      (ds/ref [:common :config :datapub])
-      {:AMI (System/getenv "DATAPUB_AMI")
-       :CloudFrontOAI (global-output :CloudFrontOAI)
-       :CredentialsKeyId (regional-output :CredentialsKeyId)
-       :CredentialsKeyUsePolicyArn (regional-output :CredentialsKeyUsePolicyArn)
-       :DatapubBucket (global-output :DatapubBucket)
-       :DatapubFilesDomainName (ds/ref [:datapub :files-domain-name])
-       :DatapubHostedZoneId (global-output :DatapubHostedZoneId)
-       :DatapubZoneApex (global-output :DatapubZoneApex)
-       :Env (config-val :env)
-       :LoadBalancerCanonicalHostedZoneId (regional-output :LoadBalancerCanonicalHostedZoneId)
-       :LoadBalancerDNSName (regional-output :LoadBalancerDNSName)
-       :LoadBalancerHTTPSListenerArn (regional-output :LoadBalancerHTTPSListenerArn)
-       :LoadBalancerSecurityGroupId (regional-output :LoadBalancerSecurityGroupId)
-       :LogsKeyArn (regional-output :LogsKeyArn)
-       :RDSSubnetGroupName (regional-output :RDSSubnetGroupName)
-       :SlackToken (System/getenv "DATAPUB_SLACK_TOKEN")
-       :VpcId (regional-output :VpcId)
-       :VpcSubnetIds (regional-output :VpcSubnetIds)})
+    {:params
+     (call (fn [env config global regional]
+             (-> config
+                 (merge
+                  (select-keys global [:CloudFrontOAI :DatapubBucket :DatapubHostedZoneId
+                                       :DatapubZoneApex])
+                  (select-keys regional [:CredentialKeysId :CredentialsKeyUsePolicyArn
+                                         :LoadBalancerCanonicalHostedZoneId :LoadBalancerDNSName
+                                         :LoadBalancerHTTPSListenerArn :LoadBalancerSecurityGroupId
+                                         :LogsKeyArn :RDSSubnetGroupName
+                                         :VpcId :VpcSubnetIds]))
+                 (assoc :AMI (System/getenv "DATAPUB_AMI")
+                        :Env env
+                        :FilesDomainName (str "files." (:DatapubZoneApex global))
+                        :SlackToken (System/getenv "DATAPUB_SLACK_TOKEN"))))
+           (ds/ref [:common :config :Env])
+           (config-val :datapub)
+           global-outputs
+           regional-outputs)
      :stack
      (cfn/stack {:capabilities capabilities
                  :lint? true
@@ -88,23 +87,22 @@
                  :parameters (ds/ref [:datapub :params])
                  :template datapub/template})}
     :graphql-gateway
-    {:stack
+    {:params
+     (call #(-> (select-keys % [:CodeBucket :SysrevHostedZoneId :SysrevZoneApex])
+                (assoc :ApolloKey (System/getenv "APOLLO_KEY")
+                       :LambdaKey (System/getenv "LAMBDA_KEY")))
+           global-outputs)
+     :stack
      (cfn/stack {:capabilities capabilities
                  :lint? true
                  :name "Sysrev-GraphQL-Gateway"
-                 :parameters {:ApolloKey (System/getenv "APOLLO_KEY")
-                              :CodeBucket (global-output :CodeBucket)
-                              :LambdaKey (System/getenv "LAMBDA_KEY")
-                              :SysrevHostedZoneId (global-output :SysrevHostedZoneId)
-                              :SysrevZoneApex (global-output :SysrevZoneApex)}
+                 :parameters (ds/ref [:graphql-gateway :params])
                  :template graphql-gateway/template})}
     :sysrev
     {:params
-     (merge-maps
-      (ds/ref [:common :config :sysrev])
-      {:CredentialsKeyId (regional-output :CredentialsKeyId)
-       :RDSSubnetGroupName (regional-output :RDSSubnetGroupName)
-       :VpcId (regional-output :VpcId)})
+     (call #(merge % (select-keys %2 [:CredentialsKeyId :RDSSubnetGroupName :VpcId]))
+           (config-val :sysrev)
+           regional-outputs)
      :stack
      (cfn/stack {:capabilities capabilities
                  :lint? true
