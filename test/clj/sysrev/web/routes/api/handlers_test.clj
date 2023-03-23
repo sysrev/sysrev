33ba21f60f6cbf0e015160c6f21ec8cfe963aa5a
John Shaffer <john@insilica.co>
2023-03-23 14:10:01 -0500

    Add clone-project-test

--- test/clj/sysrev/web/routes/api/handlers_test.clj
+++ test/clj/sysrev/web/routes/api/handlers_test.clj
@@ -1,15 +1,15 @@
 (ns sysrev.web.routes.api.handlers-test
-  (:require
-   [clojure.test :refer :all]
-   [ring.mock.request :as mock]
-   [sysrev.api :as api]
-   [sysrev.db.queries :as q]
-   [sysrev.group.core :as group]
-   [sysrev.notification.interface :as notification]
-   [sysrev.project.core :as project]
-   [sysrev.test.core :as test :refer [sysrev-handler]]
-   [sysrev.user.core :as user]
-   [sysrev.util :as util]))
+  (:require [clojure.test :refer :all]
+            [ring.mock.request :as mock]
+            [sysrev.api :as api]
+            [sysrev.db.core :as db]
+            [sysrev.db.queries :as q]
+            [sysrev.notification.interface :as notification]
+            [sysrev.project.core :as project]
+            [sysrev.project.member :as member]
+            [sysrev.test.core :as test :refer [sysrev-handler]]
+            [sysrev.user.core :as user]
+            [sysrev.util :as util]))
 
 (deftest ^:integration create-project-test
   (test/with-test-system [system {}]
@@ -48,6 +48,60 @@
                   :success true}
                (select-keys (:result project-info-response)
                             [:articles :labels :members :name :project-id :success]))))))))
+
+(deftest ^:integration clone-project-test
+  (test/with-test-system [{:keys [sr-context] :as system} {}]
+    (let [handler (sysrev-handler system)
+          test-user (test/create-test-user system)
+          user-foo (test/create-test-user system)]
+      (user/set-user-permissions (:user-id test-user) ["user" "admin"])
+      (let [api-token (-> (handler
+                           (-> (mock/request :get "/web-api/get-api-token")
+                               (mock/query-string {:email (:email test-user)
+                                                   :password (:password test-user)})))
+                          :body util/read-json :result :api-token)
+            create-project-response
+            (-> (handler
+                 (->  (mock/request :post "/web-api/create-project")
+                      (mock/body (util/write-json
+                                  {:project-name "Baz-Qux-Research"
+                                   :api-token api-token}))
+                      (mock/header "Content-Type" "application/json")))
+                :body util/read-json)
+            project-id (get-in create-project-response [:result :project :project-id])
+            _ (member/add-project-member project-id (:user-id test-user) :permissions ["member" "admin" "owner"])
+            _ (member/add-project-member project-id (:user-id user-foo) :permissions ["member" "admin"])
+            {:keys [result] :as clone-project-response}
+            (-> (handler
+                 (-> (mock/request :post "/web-api/clone-project")
+                     (mock/body (util/write-json
+                                 {:api-token api-token
+                                  :answers true
+                                  :articles true
+                                  :labels true
+                                  :members true
+                                  :new-project-name "Cloned-Project"
+                                  :project-id project-id}))
+                     (mock/header "Content-Type" "application/json")))
+                :body util/read-json)
+            clone-id (get-in clone-project-response [:result :new-project :project-id])]
+        (testing "Clone project request is successful and contains clone id"
+          (is (not (:error result)))
+          (is (:success result))
+          (is (int? clone-id)))
+        (testing "Cloned project name is correct"
+          (is (= "Cloned-Project"
+                 (-> sr-context
+                     (db/execute-one!
+                      {:select :name :from :project :where [:= :project-id clone-id]})
+                     :project/name))))
+        (testing "Members are copied and retain their permissions"
+          (is (= (:user-id test-user)
+                 (:user-id (project/get-project-owner clone-id))))
+          (is (= #{(:user-id test-user) (:user-id user-foo)}
+                 (->> (project/get-project-admins clone-id)
+                      (map :user-id)
+                      set))))))))
 
 (deftest ^:integration transfer-project-test
   (test/with-test-system [system {}]
