2e6b082d338b0b0ddfa458e98f3ff5816fdf67a5
John Shaffer <john@insilica.co>
2023-07-26 10:08:04 -0500

    Move datapub to sysrev instance

--- projects/datapub/deploy.sh
+++ projects/datapub/deploy.sh
@@ -2,5 +2,5 @@ set -e
 
 ./test.sh
 ./uberjar.sh
-rsync target/datapub.jar ubuntu@sysrev.com:/home/ubuntu/datapub/datapub.jar --progress
+rsync target/datapub.jar ubuntu@sysrev.com:/data/datapub/datapub.jar --progress
 ssh ubuntu@sysrev.com sudo systemctl restart datapub
