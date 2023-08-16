8a1fa7290329cbbb00276133f7447f6eb668bde8
Jeff Workman <jeff.workman@gmail.com>
2023-08-15 21:01:07 -0400

    Remove unused jvm-opts content from project.clj

--- project.clj
+++ project.clj
@@ -1,19 +1,8 @@
 (defproject sysrev-web "0.1.0-SNAPSHOT"
   ;; dependencies are read from deps.edn by the lein-tools-deps plugin
-
   :min-lein-version "2.8.1"
   :jvm-opts ["-Djava.util.logging.config.file=resources/logging.properties"
-             "-server"
-             ;; silence reflection warning in JVM >=10:
-             ;; "--add-opens" "java.xml/com.sun.xml.internal.stream=ALL-UNNAMED"
-             "-Xms500m" "-Xmx1000m"
-             "-XX:+TieredCompilation"
-             #_ "-XX:TieredStopAtLevel=1"
-             #_ "-XX:+AggressiveOpts"
-             #_ "-Xverify:none"
-             #_ "-XX:+UseParallelGC"
-             #_ "-XX:+UnlockExperimentalVMOptions"
-             #_ "-XX:+UseZGC"]
+             "-server"]
   :source-paths ["src/clj" "src/cljc"]
   :aliases {"build-prod" ["with-profile" "+prod" "uberjar"]
             "repl"       ["run" "-m" "sysrev.user"]}
