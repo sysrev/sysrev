58e6dccc4edf9c3fb1bd063cad53353a7ec96654
John Shaffer <john@insilica.co>
2023-09-19 18:20:33 -0500

    Download and parse open-access files from PubMed Central

--- components/pubmed-api/src/sysrev/pubmed_api/interface.clj
+++ components/pubmed-api/src/sysrev/pubmed_api/interface.clj
@@ -1,10 +1,16 @@
 (ns sysrev.pubmed-api.interface
   (:require [clojure.data.xml :as dxml]
+            [clojure.java.io :as io]
             [clojure.string :as str]
             [hato.client :as hc]
-            [sysrev.memcached.interface :as mem]))
+            [me.raynes.fs :as fs]
+            [miner.ftp :as ftp]
+            [sysrev.memcached.interface :as mem])
+  (:import [java.io FileInputStream BufferedInputStream]
+           [org.apache.commons.compress.compressors.gzip GzipCompressorInputStream]
+           [org.apache.commons.compress.archivers.tar TarArchiveInputStream]))
 
-(def default-ttl-sec 3600)
+(def default-ttl-sec 86400)
 
 ;; Rate limit is 10 requests per second per API key.
 ;; We try to limit requests to once per 100 ms to stay under on average.
@@ -41,6 +47,9 @@
 
 (defn tag-val [tag-name content]
   (some #(when (= tag-name (:tag %)) %) content))
+
+(defn tag-content [tag-name content]
+  (:content (tag-val tag-name content)))
 
 (defn- split-cached-fetches [{:keys [memcached]} pmids]
   (let [pmids-and-articles (->> pmids
@@ -90,6 +99,7 @@
         (fetch-uncached-articles opts (drop chunk-size pmids)))))))
 
 (defn get-fetches
+  "Fetch the article XML from PubMed for pmids"
   [{:as opts
     :keys [api-key hato memcached ttl-sec]
     :or {ttl-sec default-ttl-sec}}
@@ -99,3 +109,118 @@
     (concat
      cached-articles
      (fetch-uncached-articles opts uncached-pmids))))
+
+(defn article-abstract
+  "Parse the abstract from an article's XML"
+  [pubmed-article]
+  (->> pubmed-article
+       :content
+       (tag-content :MedlineCitation)
+       (tag-content :Article)
+       (tag-content :Abstract)
+       (tag-content :AbstractText)
+       (mapcat #(if (string? %) [%] (:content %)))
+       (str/join " ")))
+
+(defn article-pmcid
+  "Parse the PubMed Central ID from an article's XML"
+  [pubmed-article]
+  (->> pubmed-article
+       :content
+       (tag-content :PubmedData)
+       (tag-content :ArticleIdList)
+       (filter #(= "pmc" (:IdType (:attrs %))))
+       first
+       :content
+       first))
+
+(defn article-title
+  "Parse the title from an article's XML"
+  [pubmed-article]
+  (->> pubmed-article
+       :content
+       (tag-content :MedlineCitation)
+       (tag-content :Article)
+       (tag-content :ArticleTitle)
+       (str/join " ")))
+
+(defn get-pmc-links
+  "Get the Pubmed Central Open Access download links for a pmcid"
+  [{:keys [hato memcached ttl-sec]
+    :or {ttl-sec default-ttl-sec}}
+   pmcid]
+  (let [url "https://www.ncbi.nlm.nih.gov/pmc/utils/oa/oa.fcgi"
+        hato-opts {:http-client hato
+                   :query-params
+                   {:id (str pmcid)}
+                   :timeout 10000}
+        body (mem/cache
+              memcached
+              (str "sysrev.pubmed-api/article/get-pmc-links/" pmcid)
+              ttl-sec
+              (:body (api-get url hato-opts)))
+        content (-> body dxml/parse-str :content)
+        error (->> content (tag-val :error))
+        code (-> error :attrs :code)]
+    (cond
+      (= "idIsNotOpenAccess" code) nil
+      error (throw (ex-info (str "Error from PubMed API for " pmcid ": " code)
+                            {:code code :pmcid pmcid}))
+      :else
+      (->> body
+           dxml/parse-str
+           :content
+           (tag-content :records)
+           (tag-content :record)
+           (map :attrs)))))
+
+(defn download-pmc-file
+  "Download a Pubmed Central Open Access file"
+  [ftp-url file]
+  (let [filename (fs/base-name ftp-url)
+        url (-> ftp-url
+                (str/replace #"ftp://" "ftp://anonymous:pwd@")
+                (str/replace (re-pattern filename) ""))]
+    (ftp/with-ftp [client url
+                   :file-type :binary]
+      (ftp/client-get client filename file))))
+
+(defn get-pmc-file-nxml
+  "Returns a parsed .nxml file when given a .tar.gz containing a .nxml file"
+  [filename]
+  (with-open [file-stream (FileInputStream. filename)
+              buffered-in (BufferedInputStream. file-stream)
+              gzip-in (GzipCompressorInputStream. buffered-in)
+              tar-in (TarArchiveInputStream. gzip-in)]
+    (loop []
+      (let [entry (.getNextTarEntry tar-in)]
+        (when entry
+          (if (str/ends-with? (.getName entry) ".nxml")
+            (-> tar-in io/reader slurp (dxml/parse-str {:support-dtd false}))
+            (recur)))))))
+
+(defn html->plaintext
+  "Returns a seq of Strings represeting a plaintext version of html.
+   The html should be in XML-style :tag :content :attr maps."
+  [html]
+  (cond
+    (string? html) [html]
+    (map? html)
+    #__ (let [{:keys [content tag]} html]
+          (cond
+            (= :xref tag) nil
+            (= :p tag) (concat
+                        (mapcat html->plaintext content)
+                        ["\n\n"])
+            :else (mapcat html->plaintext content)))
+    (seqable? html) (mapcat html->plaintext html)))
+
+(defn nxml-text
+  "Returns a String representing an plaintext version of an nxml
+   article's full text."
+  [nxml]
+  (->> nxml
+       :content
+       (tag-val :body)
+       html->plaintext
+       (str/join "")))
--- components/pubmed-api/test/sysrev/pubmed_api/interface_test.clj
+++ components/pubmed-api/test/sysrev/pubmed_api/interface_test.clj
@@ -1,8 +1,23 @@
 (ns sysrev.pubmed-api.interface-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [babashka.fs :as fs]
+            [clojure.test :refer [deftest is]]
             [com.stuartsierra.component :as component]
+            [sysrev.file-util.interface :as file-util]
             [sysrev.memcached.interface :as mem]
             [sysrev.pubmed-api.interface :as pubmed-api]))
+
+(defmacro with-system [[system-sym _opts] & body]
+  `(let [system-map# (component/system-map
+                      :memcached (component/using
+                                  (mem/temp-client)
+                                  {:server :memcached-server})
+                      :memcached-server (mem/temp-server))
+         system# (component/start system-map#)
+         ~system-sym system#]
+    (try
+      ~@body
+      (finally
+        (component/stop system#)))))
 
 (defn get-article-id [xml]
     (->> xml
@@ -15,22 +30,34 @@
          :content))
 
 (deftest test-get-fetches
-  (let [system-map (component/system-map
-                    :memcached (component/using
-                                (mem/temp-client)
-                                {:server :memcached-server})
-                    :memcached-server (mem/temp-server))
-        system (component/start system-map)
-        opts (assoc system :ttl-sec 86400)
-        angry-bees-pmids ["31203899" "31021106" "24860522" "24427187" "16999303"]]
-    (try
+  (with-system [system {}]
+    (let [angry-bees-pmids ["31203899" "31021106" "24860522" "24427187" "16999303"]]
       (is (= angry-bees-pmids
-             (->> (pubmed-api/get-fetches opts angry-bees-pmids)
+             (->> (pubmed-api/get-fetches system angry-bees-pmids)
                   (mapcat get-article-id)))
           "uncached fetch")
       (is (= angry-bees-pmids
-             (->> (pubmed-api/get-fetches opts angry-bees-pmids)
+             (->> (pubmed-api/get-fetches system angry-bees-pmids)
                   (mapcat get-article-id)))
-          "cached fetch")
-      (finally
-        (component/stop system)))))
+          "cached fetch"))))
+
+(deftest test-get-pmc-links
+  (with-system [system {}]
+    (is (nil? (pubmed-api/get-pmc-links system "PMC7170517"))
+        "article with no open-access links")
+    (is (= {:format "tgz"
+            :href "ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_package/fc/a8/PMC4741700.tar.gz"}
+           (-> (pubmed-api/get-pmc-links system "PMC4741700")
+               first
+               (select-keys [:format :href])))
+        "article with one open-access link")))
+
+(deftest test-pmc-file-nxml
+  (file-util/with-temp-file [PMC4741700 {:prefix "sysrev-pubmed-api"
+                                         :suffix ".tar.gz"}]
+    (pubmed-api/download-pmc-file "ftp://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_package/fc/a8/PMC4741700.tar.gz" (fs/file PMC4741700))
+    (is (= "INTRODUCTION"
+           (-> (pubmed-api/get-pmc-file-nxml (fs/file PMC4741700))
+               pubmed-api/nxml-text
+               (subs 0 12)))
+        "Article nxml is downloaded and parsed to plaintext")))
